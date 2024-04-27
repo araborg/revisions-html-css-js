3265d1ea345ed665549cd4fc3b6008511e49fe1f
arabog <arababs2015@gmail.com>
2024-04-27 05:02:12 +0100

    JS: Course1: Class1: Learning continue

--- revision-25-4-24/js/course1/pyramid-generator/script.js
+++ revision-25-4-24/js/course1/pyramid-generator/script.js
@@ -2,3 +2,7 @@ let character = "Hello";
 let count = 8;
 character[1] = "a";
 console.log(character);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
