85c985624eebe025f5cad6f49136a86d4e5edbb6
arabog <arababs2015@gmail.com>
2024-04-30 18:57:22 +0100

    JS: Course1: Class1: pyramid generator 10

--- revision-25-4-24/js/courses/course1/project/class1-project/script.js
+++ revision-25-4-24/js/courses/course1/project/class1-project/script.js
@@ -6,8 +6,12 @@ for (let i = 0; i < target; i++) {
     layers.push(character.repeat(i));
 }
 
-for (let i = 0; i < layers.length; i++) {
-    console.log(layers[i]);
+// for (let i = 0; i < layers.length; i++) {
+//     console.log(layers[i]);
+// }
+
+for (const layer of layers) {
+    console.log(layer);
 }
 
 // console.log(layers);
