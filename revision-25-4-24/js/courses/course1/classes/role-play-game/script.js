785396a5d7f220982227feb047d8e3c8ba5a32bc
arabog <arababs2015@gmail.com>
2024-05-08 10:38:06 +0100

    JS Review: Course2: Class2: Updated Lesson 92

--- revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
+++ revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
@@ -124,6 +124,8 @@ function buyWeapon() {
         text.innerText = "You now have a " + newWeapon + ".";
 
         inventory.push(newWeapon);
+
+        text.innerText += " In your inventory you have: ";
     }
 }
 
