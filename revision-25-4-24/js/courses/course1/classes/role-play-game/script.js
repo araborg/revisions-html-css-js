275590602bf60405f30d34a96dd3d194e8e7ba14
arabog <arababs2015@gmail.com>
2024-05-08 09:33:01 +0100

    JS Review: Course2: Class2: Updated Lesson 77

--- revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
+++ revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
@@ -62,11 +62,13 @@ function fightDragon() {
 }
 
 function buyHealth() {
-    gold -= 10;
-    health += 10;
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
 
-    goldText.innerText = gold;
-    healthText.innerText = health;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    }
 }
 
 function buyWeapon() {}
