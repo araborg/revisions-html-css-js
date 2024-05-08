3e5dd16448906e6ed645fb758a7a2abfe25e3744
arabog <arababs2015@gmail.com>
2024-05-08 07:56:38 +0100

    JS Review: Course2: Class2: Updated Lesson 65

--- revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
+++ revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
@@ -60,7 +60,7 @@ function buyHealth() {}
 function buyWeapon() {}
 
 function update(location) {
-    button1.innerText = location["button text"];
+    button1.innerText = location["button text"][0];
     button2.innerText = "Go to cave";
     button3.innerText = "Fight dragon";
 
