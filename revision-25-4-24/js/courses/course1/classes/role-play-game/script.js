4278c96a298d48c962de8a5513ad7d3f29829fa0
arabog <arababs2015@gmail.com>
2024-05-10 09:39:38 +0100

    JS Review: Course1: Class2: Updated Lesson 127

--- revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
+++ revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
@@ -254,7 +254,9 @@ function attack() {
     }
 }
 
-function dodge() {}
+function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
 
 function defeatMonster() {}
 
