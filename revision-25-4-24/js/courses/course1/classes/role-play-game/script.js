73d566515004d29042a0df05fa339b3e502a1207
arabog <arababs2015@gmail.com>
2024-05-11 23:47:58 +0100

    JS Review: Course1: Class2: Updated Lesson 166

--- revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
+++ revision-25-4-24/js/courses/course1/classes/role-play-game/script.js
@@ -357,9 +357,11 @@ function pick(guess) {
 
     while (numbers.length < 10) {
         numbers.push(Math.floor(Math.random() * 11));
-        text.innerText =
-            "You picked " + guess + ". Here are the random numbers:\n";
     }
+
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+
+    for (let i = 0; i < 10; i++) {}
 }
 
 function pickTwo() {
