d85f54ac82b9b941a43deef4e7907985ec0b83a5
arabog <arababs2015@gmail.com>
2024-05-21 11:58:42 +0100

    JS Project: Course1: Class2: Role-play project 159

--- revision-25-4-24/projects/JS/role-play-game/script.js
+++ revision-25-4-24/projects/JS/role-play-game/script.js
@@ -182,13 +182,13 @@ function goCave() {
 
 function buyHealth() {
     if (gold >= 10) {
-        playerHealth += 10;
         gold -= 10;
+        playerHealth += 10;
 
         playerHealthText.innerText = playerHealth;
         goldText.innerText = gold;
     } else {
-        text.innerText = "You do not have enough gold to buy health.";
+        text.innerText = `You do not have enough gold ${gold} to buy health.`;
     }
 }
 
