cdbe633a50df46f5a64faa55fd7267450c8ca974
arabog <arababs2015@gmail.com>
2024-05-23 10:39:29 +0100

    JS Project: Course1: Class3: Calorie Counter JS: Lesson 16b

--- revision-25-4-24/projects/JS/calorie-counter/script.js
+++ revision-25-4-24/projects/JS/calorie-counter/script.js
@@ -1,5 +1,8 @@
 const calorieCounter = document.getElementById("calorie-counter");
+
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entryDropdown");
+
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
