4d0f0726d36df8a949bbf743d181a20783c41716
arabog <arababs2015@gmail.com>
2024-05-25 03:53:02 +0100

    JS Project: Course1: Class3: Calorie Counter JS:Updated tutorial-script.js Lesson 63

--- revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
+++ revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
@@ -113,6 +113,7 @@ function getCaloriesFromInputs(list) {
         const invalidInputMatch = isInvalidInput(currVal);
 
         if (invalidInputMatch) {
+            alert("Invalid Input" + invalidInputMatch[0]);
         }
     }
 }
