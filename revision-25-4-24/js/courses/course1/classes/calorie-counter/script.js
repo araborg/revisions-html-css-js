3c89432d3ffd2c671cc755c61c8062422c4a8e6b
arabog <arababs2015@gmail.com>
2024-05-25 12:13:45 +0100

    JS Project: Course1: Class3: Calorie Counter JS:Updated Lesson 72

--- revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
+++ revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
@@ -131,11 +131,25 @@ function calculateCalories(e) {
     e.preventDefault();
     isError = false;
 
-    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type="number"]');
-    const lunchNumberInputs = document.querySelectorAll('#lunch input[type="number"]');
-    const dinnerNumberInputs = document.querySelectorAll('#dinner input[type="number"]');
-    const snacksNumberInputs = document.querySelectorAll('#snacks input[type="number"]');
-    const exerciseNumberInputs = document.querySelectorAll('#exercise input[type="number"]');
+    // inputs
+    const breakfastNumberInputs = document.querySelectorAll(
+        '#breakfast input[type="number"]'
+    );
+    const lunchNumberInputs = document.querySelectorAll(
+        '#lunch input[type="number"]'
+    );
+    const dinnerNumberInputs = document.querySelectorAll(
+        '#dinner input[type="number"]'
+    );
+    const snacksNumberInputs = document.querySelectorAll(
+        "#snacks input[type=number]"
+    );
+    const exerciseNumberInputs = document.querySelectorAll(
+        "#exercise input[type=number]"
+    );
+
+    // calling fxn
+    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
 
     // console.log(breakfastNumberInputs);
 }
