e6037e3ff8e0a9dc73479b8df49f6b26bc6bc6c9
arabog <arababs2015@gmail.com>
2024-05-25 03:49:45 +0100

    JS Project: Course1: Class3: Calorie Counter JS:Updated tutorial-script.js Lesson 60

--- revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
+++ revision-25-4-24/js/courses/course1/classes/calorie-counter/script.js
@@ -109,6 +109,6 @@ function getCaloriesFromInputs(list) {
     let calories = 0;
 
     for (const item of list) {
-        const currVal = item.value;
+        const currVal = cleanInputString(item.value);
     }
 }
