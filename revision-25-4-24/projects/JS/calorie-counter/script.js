8c7f5e4ef28c0a5dab5b37ee9375e8253b426cd1
arabog <arababs2015@gmail.com>
2024-05-31 21:00:18 +0100

    JS Class1: Lecture3: Calorie-Counter: 48

--- revision-25-4-24/projects/JS/calorie-counter/script.js
+++ revision-25-4-24/projects/JS/calorie-counter/script.js
@@ -1,6 +1,7 @@
 const budgetInput = document.querySelector("#budget");
 const entryDropdown = document.querySelector("#entry-dropdown");
+
 const calorieCounter = document.querySelector("#calorie-counter");
 const addEntry = document.getElementById("add-entry");
 const clearBtn = document.querySelector("#clear");
-console.log(addEntry);
+console.log(calorieCounter, addEntry, clearBtn);
