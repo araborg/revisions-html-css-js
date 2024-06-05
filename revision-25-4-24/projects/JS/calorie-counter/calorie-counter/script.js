64a2014cac3447b9eb317d17891bbf35ac62fb6c
arabog <arababs2015@gmail.com>
2024-06-05 09:22:48 +0100

    JS Class1: Lecture3: Calorie-Counter Remake: JS updated 20

--- revision-25-4-24/projects/JS/calorie-counter/calorie-counter/script.js
+++ revision-25-4-24/projects/JS/calorie-counter/calorie-counter/script.js
@@ -1,8 +1,18 @@
 const budget = document.querySelector("#budget");
-
 const output = document.querySelector("#output");
+
 // btns
 const caloriesCounter = document.querySelector("#calories-counter");
 const addEntryBtn = document.querySelector("#add-entry");
 const clearBtn = document.querySelector("#clear");
-console.log(budget, output, caloriesCounter, addEntryBtn, clearBtn);
+
+// state
+let isError = false;
+
+// Helper fxns
+function trimInput(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, "");
+}
+
+console.log(trimInput(" +- 200 + - "));
