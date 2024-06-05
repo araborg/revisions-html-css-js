8d97e7be4905fb83a5dacd9ea2234b9a7c388582
arabog <arababs2015@gmail.com>
2024-06-05 10:39:01 +0100

    JS Class1: Lecture3: Calorie-Counter Remake: JS updated 32

--- revision-25-4-24/projects/JS/calorie-counter/calorie-counter/script.js
+++ revision-25-4-24/projects/JS/calorie-counter/calorie-counter/script.js
@@ -33,20 +33,19 @@ const addInput = () => {
     const inputContainer = document.querySelector(
         `#${selectOption.value} .input-container`
     );
-    console.log(inputContainer);
+    // console.log(inputContainer);
 
     // get d length of d inputs in an input container
     const inputContainerLength =
         inputContainer.querySelectorAll(".input-container").length + 1;
-    console.log(inputContainerLength);
+    // console.log(inputContainerLength);
 
     const inputHTML = `
-        <label for="${selectOption.value}-1-name"> Entry ${1} Name</label>
+        <label for="${selectOption.value}-${inputContainerLength}-name"> Entry ${inputContainerLength} Name</label>
         <input type="text" id="" placeholder="Name" />
 
-        <label for="${
-            selectOption.value
-        }-1-calories"> Entry ${1} Calories</label>
+
+        <label for="${selectOption.value}-${inputContainerLength}-calories"> Entry ${inputContainerLength} Calories</label>
         <input type="number" placeholder="Calories" />
 
     `;
