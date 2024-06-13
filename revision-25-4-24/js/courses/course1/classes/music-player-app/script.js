3dbc57b5d407bef658bf349d172651573b5dc016
arabog <arababs2015@gmail.com>
2024-06-13 10:37:29 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 5

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -81,7 +81,11 @@ const allSongs = [
 ];
 
 const playlistSongs = document.getElementById("playlist-songs");
+
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
 
 console.log(playlistSongs, playButton, pauseButton);
