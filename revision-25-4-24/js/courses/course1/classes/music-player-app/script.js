e5e821b5700e85b761b3d6cecb69ecb0f5077179
arabog <arababs2015@gmail.com>
2024-06-13 12:56:04 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 20

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -146,8 +146,6 @@ const printGreeting = () => {
 
 printGreeting();
 
-const addTwoNumbers = (num1, num2) => {
-    return num1 * num2;
-};
+const addTwoNumbers = (num1, num2) => num1 + num2;
 
 console.log(addTwoNumbers(3, 4));
