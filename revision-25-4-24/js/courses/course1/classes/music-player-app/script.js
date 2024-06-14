d4e8b19f5f54f20edc17896f7e39c4b3b928b6db
arabog <arababs2015@gmail.com>
2024-06-14 08:54:21 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 37

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -192,5 +192,15 @@ const renderSongs = (array) => {
 renderSongs(userData?.songs);
 
 const sortSongs = () => {
-    userData?.songs.sort((a, b) => {});
+    userData?.songs.sort((a, b) => {
+        if (a.title < b.title) -1;
+
+        if (a.title > b.title) {
+            return 1;
+        }
+
+        if (a.title === b.title) 0;
+    });
+
+    return userData?.songs;
 };
