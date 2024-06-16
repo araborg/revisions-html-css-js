87c9dc00bbb6331af5efb927e3fd4baf46232a69
arabog <arababs2015@gmail.com>
2024-06-16 16:32:06 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 93

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -317,8 +317,11 @@ const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title ");
     const songArtist = document.getElementById("player-song-artist");
 
-    const currentTitle = userData?.currentSong.title;
-    const currentArtist = userData?.currentSong.artist;
+    const currentTitle = userData?.currentSong?.title;
+    const currentArtist = userData?.currentSong?.artist;
+
+    playingSong.textContent = currentTitle ? currentTitle : "";
+    songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 //
