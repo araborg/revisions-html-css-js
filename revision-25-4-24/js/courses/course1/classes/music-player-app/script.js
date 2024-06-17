1935983a1399d7c7952f091843565dade976b2d2
arabog <arababs2015@gmail.com>
2024-06-17 08:38:31 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 10~

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -340,6 +340,9 @@ const setPlayButtonAccessibleText = () => {
 
 const shuffle = () => {
     userData?.songs.sort(() => Math.random() - 0.5);
+
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
 };
 
 //
