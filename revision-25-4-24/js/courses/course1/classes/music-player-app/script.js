1e42b7f21e6fddaf0802cf7d8b2b8a6c935c3fee
arabog <arababs2015@gmail.com>
2024-06-17 08:16:20 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 97

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -327,6 +327,11 @@ const setPlayerDisplay = () => {
 
 const setPlayButtonAccessibleText = () => {
     const song = userData?.currentSong || userData.songs[0];
+
+    playButton.setAttribute(
+        "aria-label",
+        song?.title ? `Play ${song.title}` : "Play"
+    );
 };
 
 //
