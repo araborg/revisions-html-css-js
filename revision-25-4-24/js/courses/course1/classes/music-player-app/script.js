bf67b85d3b4176b94f06e339e8ad0fbe50a302d9
arabog <arababs2015@gmail.com>
2024-06-17 11:12:19 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 133

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -196,6 +196,20 @@ const renderSongs = (array) => {
                         </span>
                     </button>
 
+                    <button>
+                        <svg
+                            width="24"
+                            height="19"
+                            viewBox="0 0 48 48"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0 0h48v48H0z" fill="none" />
+                            <path
+                                d="M24 10V2L14 12l10 10v-8c6.63 0 12 5.37 12 12s-5.37 12-12 12-12-5.37-12-12H8c0 8.84 7.16 16 16 16s16-7.16 16-16-7.16-16-16-16z"
+                            />
+                        </svg>
+                    </button>
+
                     <button 
                         class="playlist-song-delete"
                         arial-label="Delete ${song.title}"
@@ -217,6 +231,8 @@ const renderSongs = (array) => {
                             />
                         </svg>
                     </button>
+
+                
                 </li>
         `;
         })
