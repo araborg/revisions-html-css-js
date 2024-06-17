50e245ededf4d4e4921b12a9125c3c6a8fc30008
arabog <arababs2015@gmail.com>
2024-06-17 10:45:25 +0100

    JS Class1: Lecture4: Music Player App: Updated JS 129

--- revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/script.js
@@ -401,6 +401,11 @@ audio.addEventListener("ended", () => {
     } else {
         userData.currentSong = null;
         userData.songCurrentTime = 0;
+
+        pauseSong();
+        setPlayerDisplay();
+        highlightCurrentSong();
+        setPlayButtonAccessibleText();
     }
 });
 
