54bbc7c2e50a2eeaeb15bff480f822c8ae952039
arabog <arababs2015@gmail.com>
2024-06-19 12:36:56 +0100

    JS Class1: Lecture4: Music Player App Remake: HTML 1

---
+++ revision-25-4-24/projects/JS/music-player-app/script.js
@@ -0,0 +1,18 @@
+/*
+const userData
+const sortSongs
+const renderSongs
+const playSong
+const pauseSong
+const getCurrentSongIndex
+const playNextSong
+const playPreviousSong
+const highlightCurrentSong
+const setPlayerDisplay
+const setPlayButtonAccessibleText
+const shuffle
+const replayAllSongs
+const replaySong
+const deleteSong
+
+*/
