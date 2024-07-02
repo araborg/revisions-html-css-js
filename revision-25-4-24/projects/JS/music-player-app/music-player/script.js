c7e2feaa72143724b0a1393cf6209677999f4d7f
arabog <arababs2015@gmail.com>
2024-07-02 12:56:08 +0100

    Music player project JS init

--- revision-25-4-24/projects/JS/music-player-app/music-player/script.js
+++ revision-25-4-24/projects/JS/music-player-app/music-player/script.js
@@ -1,5 +1,7 @@
 // btns
 const previousBtn = document.getElementById("previous");
 const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+const nextBtn = document.getElementById("next");
 
-console.log(playBtn);
+console.log(nextBtn);
