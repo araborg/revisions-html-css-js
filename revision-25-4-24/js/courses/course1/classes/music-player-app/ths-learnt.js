0baf754524ee418079c5ae064226738bdb281b8f
arabog <arababs2015@gmail.com>
2024-07-10 21:03:47 +0100

    Music player project HTML 240

---
+++ revision-25-4-24/js/courses/course1/classes/music-player-app/ths-learnt.js
@@ -0,0 +1,47 @@
+/*
+Ths learnt:
+1. arrow fxn (() => {}) is an anonymous fxn expression
+2. optional chaining (?.) for nested obj ppties dt might be null or undefined.
+
+3. why .join() method is used on .map() method
+4. .find() method returns d 1st elemt dt fulfilled d stipulated condition
+
+5. .indexOf()
+6. .removeAttribute()
+
+7. .setAttribute(arg1, arg2)
+8. Math.random() - 0.5
+
+9. .filter()
+10. .createElement()
+
+11. .createTextNode()
+12. .appendChild()
+
+13. .remove()
+14. ended event
+
+15. setPlayButtonAccessibleText() called in: 
+    playSong()
+    shuffle()
+    deleteSong()
+        resetButton()
+    audio.addEventListener()
+
+16. 
+const userData
+const sortSongs
+const renderSongs
+const playSong
+const pauseSong
+const getCurrentSongIndex
+const playNextSong
+const playPreviousSong
+const highlightCurrentSong
+const setPlayerDisplay
+const setPlayButtonAccessibleText
+const shuffle
+const replayAllSongs
+const replaySong
+const deleteSong
+*/
