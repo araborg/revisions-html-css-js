7e1648f338ce24aacb96c4b4d3fd9a6d945046fc
arabog <arababs2015@gmail.com>
2024-07-24 05:06:54 +0100

    updated Media Player App HTML, CSS, JS 333

--- revision-25-4-24/class-projects/JS/music-player-app/script.js
+++ revision-25-4-24/class-projects/JS/music-player-app/script.js
@@ -239,8 +239,11 @@ const shuffle = () => {
 shuffleButton.addEventListener("click", shuffle);
 
 const deleteSong = (id) => {
+    // /const songs =
     userData?.songs.filter((song) => song.id !== id);
-    console.log(userData?.songs);
+
+    renderSongs(userData?.songs);
+    console.log(songs);
 };
 
 // const highlightCurrentSong
