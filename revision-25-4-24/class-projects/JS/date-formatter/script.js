9f5adf66b974ea4970e0d8788d404c72cbb7735f
arabog <arababs2015@gmail.com>
2024-07-30 10:51:09 +0100

    Date Formatter Project HTML, CSS, JS 16

--- revision-25-4-24/class-projects/JS/date-formatter/script.js
+++ revision-25-4-24/class-projects/JS/date-formatter/script.js
@@ -7,6 +7,8 @@ const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 
+const hours = date.getHours();
+
 // const newYear = date.getUTCFullYear
 
 console.log(month);
