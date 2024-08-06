9c7e3f29b42b2067917d56442db020785dcea825
arabog <arababs2015@gmail.com>
2024-08-06 20:08:36 +0100

    date-formatter project HTML, CSS, JS 67

--- revision-25-4-24/js/courses/course1/classes/date-formatter/script.js
+++ revision-25-4-24/js/courses/course1/classes/date-formatter/script.js
@@ -1,7 +1,12 @@
 const selectDate = document.getElementById("date-format");
 
+const weekdays = [];
 const date = new Date();
-console.log(date);
+
+const day = date.getDate();
+const month = date.getDay();
+
+console.log(day, month);
 
 selectDate.addEventListener("change", function () {
     console.log(selectDate.value);
