4fdb0d3237bcdfe73d7e2b50ec5c0107966b966d
arabog <arababs2015@gmail.com>
2024-08-18 17:46:46 +0100

    Capstone JS Project 24

--- capstone-projects/class1-project/js-palindrome-checker/script.js
+++ capstone-projects/class1-project/js-palindrome-checker/script.js
@@ -28,31 +28,32 @@ const removeUnwantedChar = (str) => {
     console.log(str.replace(regex, ""));
 };
 
-removeUnwantedChar("A");
-removeUnwantedChar("eye");
-removeUnwantedChar("_eye");
-removeUnwantedChar("A man, a plan, a canal. Panama");
-removeUnwantedChar("never odd or even");
-removeUnwantedChar("My age is 0, 0 si ega ym.");
+// removeUnwantedChar("A");
+// removeUnwantedChar("eye");
+// removeUnwantedChar("_eye");
+// removeUnwantedChar("A man, a plan, a canal. Panama");
+// removeUnwantedChar("never odd or even");
+// removeUnwantedChar("My age is 0, 0 si ega ym.");
 
-removeUnwantedChar("0_0 (: /- :) 0-0");
+// removeUnwantedChar("0_0 (: /- :) 0-0");
 // console.log();
 
-removeUnwantedChar("not a palindrome");
-removeUnwantedChar("nope");
-removeUnwantedChar("almostomla");
-removeUnwantedChar("1 eye for of 1 eye.");
-removeUnwantedChar("five|_/|four");
+// removeUnwantedChar("not a palindrome");
+// removeUnwantedChar("nope");
+// removeUnwantedChar("almostomla");
+// removeUnwantedChar("1 eye for of 1 eye.");
+// removeUnwantedChar("five|_/|four");
 
 function check() {
     const textInputValue = textInput.value;
-    console.log(textInputValue);
+    const inputValue = removeUnwantedChar(textInputValue);
 
     if (!textInput.value) {
         alert("Please input a value");
     } else {
-        const reverseTextInput = textInputValue.split("").reverse().join(" ");
-        console.log(reverseTextInput);
+        console.log(inputValue);
+        // const reverseTextInput = textInputValue.split("").reverse().join(" ");
+        // console.log(reverseTextInput);
     }
 }
 
