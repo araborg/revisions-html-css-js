ec5a344ca2c59f432cc0daa57fc109bba2a6f6c2
arabog <arababs2015@gmail.com>
2024-08-18 16:58:01 +0100

    Capstone JS Project 12

--- capstone-projects/class1-project/js-palindrome-checker/script.js
+++ capstone-projects/class1-project/js-palindrome-checker/script.js
@@ -4,8 +4,10 @@ const btn = document.getElementById("check-btn");
 function check() {
     if (!textInput.value) {
         alert("Please input a value");
+    } else {
+        const reverseTextInput = textInput.reverse();
+        console.log(reverseTextInput);
     }
-    console.log(textInput.value);
 }
 
 btn.addEventListener("click", check);
