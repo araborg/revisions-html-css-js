809b20078049281c2fbc2f40c2c3ac9bb6cd7ee8
arabog <arababs2015@gmail.com>
2024-08-18 20:49:50 +0100

    Capstone JS Project 32

--- capstone-projects/class1-project/js-palindrome-checker/script.js
+++ capstone-projects/class1-project/js-palindrome-checker/script.js
@@ -8,22 +8,6 @@ const removeUnwantedChar = (str) => {
     return str.replace(regex, "");
 };
 
-// removeUnwantedChar("A");
-// removeUnwantedChar("eye");
-// removeUnwantedChar("_eye");
-// removeUnwantedChar("A man, a plan, a canal. Panama");
-// removeUnwantedChar("never odd or even");
-// removeUnwantedChar("My age is 0, 0 si ega ym.");
-
-// removeUnwantedChar("0_0 (: /- :) 0-0");
-// console.log();
-
-// removeUnwantedChar("not a palindrome");
-// removeUnwantedChar("nope");
-// removeUnwantedChar("almostomla");
-// removeUnwantedChar("1 eye for of 1 eye.");
-// removeUnwantedChar("five|_/|four");
-
 function check() {
     const textInputValue = textInput.value;
     const inputValue = removeUnwantedChar(textInputValue).toLowerCase();
