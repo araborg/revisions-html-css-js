ac144bbf8f726ef98b68adeb149872a74504daf2
arabog <arababs2015@gmail.com>
2024-11-01 01:53:41 +0100

    updating freecodecamp 7

--- revision-25-4-24/js-practice/courses/course2/filter-players/script.js
+++ revision-25-4-24/js-practice/courses/course2/filter-players/script.js
@@ -20,4 +20,9 @@ const myFavoriteFootballTeam = {
     sport: "Football",
     year: 1986,
     isWorldCupWinner: true,
+
+    headCoach: {
+        coachName: "Carlos Bilardo",
+        matches: 7,
+    },
 };
