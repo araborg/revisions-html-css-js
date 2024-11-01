356e1a9a21bdabcc22c6cf797673faf9622db2b6
arabog <arababs2015@gmail.com>
2024-11-01 02:12:23 +0100

    updating freecodecamp 19

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -199,5 +199,33 @@ returns the same object that was passed in.
 
 
 2. Object destructuring:
+const sport = myFavoriteFootballTeam.sport;
+const team = myFavoriteFootballTeam.team;
+
+In the last two steps, you have been accessing properties 
+from the myFavoriteFootballTeam object using dot notation 
+and assigning them to new const variables. But in JavaScript, 
+there is an easier way to accomplish the same goal.
+
+The object destructuring syntax allows you to unpack values 
+from arrays and objects:
+
+const developerObj = {
+    name: "Jessica Wilkins",
+    isDeveloper: true
+};
+
+// Object destructuring
+const { name, isDeveloper } = developerObj;
+
+Rewrite the two lines of code below using the new 
+destructuring syntax. Your answer should be one line of code.
+
+const sport = myFavoriteFootballTeam.sport;
+const team = myFavoriteFootballTeam.team;
+
+const {sport, team} = myFavoriteFootballTeam;
+
+
 
 */
