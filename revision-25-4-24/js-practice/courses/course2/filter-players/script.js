94e6c380758bc20c217193a4d619dcef5dd802dd
arabog <arababs2015@gmail.com>
2024-11-01 03:31:00 +0100

    updating freecodecamp 42

--- revision-25-4-24/js-practice/courses/course2/filter-players/script.js
+++ revision-25-4-24/js-practice/courses/course2/filter-players/script.js
@@ -220,7 +220,31 @@ headCoach.textContent = coachName;
 
 // Logic:
 const setPlayerCards = (arr = players) => {
-    playerCards.innerHTML += arr.map().join("");
+    playerCards.innerHTML += arr
+        .map(({ name, position, number, isCaptain, nickname }) => {})
+        .join("");
+
+    /*
+    
+    ({ name, position, number, isCaptain, nickname }) => {
+            `
+                <div class="player-card">
+                    <h2>
+                        ${isCaptain ? "(Captain)" : ""} 
+                        ${name}
+                    </h2>
+
+                    <p>Position: ${position}</p>
+
+                    <p>Number: ${number}</p>
+
+                    <p>
+                        Nickname: ${nickname !== null ? nickname : "N/A"}
+                    </p>
+                </div>
+            `;
+        }
+    */
 };
 
 // console.log(setPlayerCards(players));
