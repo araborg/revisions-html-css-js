1c7a0ea40f639122859c4b488e1b0325c443633c
arabog <arababs2015@gmail.com>
2024-11-01 02:04:41 +0100

    updating freecodecamp 14

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -177,6 +177,16 @@ timer and stop the code execution beforehand.
 
 ===================================
 Football Team Cards:
+1. Object.freeze():
+The next step is to ensure that you can't modify this object by 
+adding or removing any properties. We are going to use a method 
+called Object.freeze(obj) which will freeze this object and 
+prevent any changes being made to it.
+
+Use the Object.freeze() method to freeze the myFavoriteFootballTeam 
+object.
+
+Object.freeze(myFavoriteFootballTeam);
 
 
 
