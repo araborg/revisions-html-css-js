0850eccb843e0384c99d59443786a7cb557cff95
arabog <arababs2015@gmail.com>
2024-11-01 01:45:30 +0100

    updating freecodecamp 5

--- revision-25-4-24/js-practice/courses/course2/filter-players/script.js
+++ revision-25-4-24/js-practice/courses/course2/filter-players/script.js
@@ -4,5 +4,7 @@ const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 
-const playerCard = document.getElementById("player-card");
+const playerCards = document.getElementById("player-card");
+console.log(teamName);
+const playersDropdownList = document.getElementById("players");
 console.log(teamName);
