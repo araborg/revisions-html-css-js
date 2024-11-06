21db5ca6eea4584898ceffd40b3b466c65237c09
arabog <arababs2015@gmail.com>
2024-11-06 12:51:25 +0100

    Todo List: 10

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -5,7 +5,9 @@ const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
-const tasksContainer = document.getElementById("tasks-container");
+
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+const tasksContainer = document.getElementById("tasks-container");
