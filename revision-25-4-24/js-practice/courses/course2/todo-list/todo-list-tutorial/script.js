db2ae2ceb74c992308545b8d203162e32e4c07b7
arabog <arababs2015@gmail.com>
2024-11-06 12:56:40 +0100

    Todo List: 16

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -2,16 +2,15 @@ const openTaskFormBtn = document.getElementById("open-task-form-btn");
 
 const taskForm = document.getElementById("task-form");
 
-const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
-
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
-
-const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
 
