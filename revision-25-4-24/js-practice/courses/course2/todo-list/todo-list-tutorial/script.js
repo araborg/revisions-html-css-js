71fafacc05697912121e7e3bfffe85e09577ea67
arabog <arababs2015@gmail.com>
2024-11-06 12:55:45 +0100

    Todo List: 15

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -2,9 +2,9 @@ const openTaskFormBtn = document.getElementById("open-task-form-btn");
 
 const taskForm = document.getElementById("task-form");
 
-const closeTaskFormBtn = document.getElementById("close-task-form-btn");
-
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 
