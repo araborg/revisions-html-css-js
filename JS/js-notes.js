21341c7aa730fca5e5b2d8d474ff87ef4f8c5977
arabog <arababs2015@gmail.com>
2024-11-07 05:12:19 +0100

    tutorial-script.js Todo List  4

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -359,11 +359,16 @@ associated data, including title, due date, and description.
 This storage will enable you to keep track of tasks, display 
 them on the page, and save them to localStorage.
 
-let currentTask = {};
-
 
 
 5.
+let currentTask = {};
+
+Use let to create a currentTask variable and set it to an 
+empty object. 
+
+This variable will be used to track the state when editing 
+and discarding tasks.
 
 
 6.
