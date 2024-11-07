1f915bfa316cf0cba5d4f3eca82e209edf5a9c94
arabog <arababs2015@gmail.com>
2024-11-07 06:04:47 +0100

    Todo List  22

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -95,6 +95,10 @@ const reset = () => {
     titleInput.value = "";
     dateInput.value = "";
     descriptionInput.value = "";
+
+    taskForm.classList.toggle("hidden");
+
+    currentTask = {};
 };
 
 //
