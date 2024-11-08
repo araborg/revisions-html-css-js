8a37c0b23472861710c16017a8eb52160134f048
arabog <arababs2015@gmail.com>
2024-11-08 11:01:32 +0100

    updated: Todo List 33

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -187,6 +187,13 @@ const editTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex(
         (item) => item.id === buttonEl.parentElement.id
     );
+
+    currentTask = taskData[dataArrIndex];
+    console.log(currentTask);
+
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
 };
 
 //
