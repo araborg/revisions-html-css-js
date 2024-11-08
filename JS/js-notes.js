0604b5da0f30a48096ae5061b8caee1e39008a56
arabog <arababs2015@gmail.com>
2024-11-08 11:16:29 +0100

    updated: Todo List 39

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -485,6 +485,18 @@ localStorage.setItem("data", JSON.stringify(myTaskArr));
 
 
 14.
+Now that you have the myTaskArr array saved in localStorage 
+correctly, you can retrieve it with getItem() by specifying 
+the key you used to save the item.
+
+Use the getItem() method to retrieve the myTaskArr array 
+and assign it to the variable getTaskArr. Then, log the 
+getTaskArr variable to the console to see the result.
+
+localStorage.setItem("data", JSON.stringify(myTaskArr));
+
+const getTaskArr = localStorage.getItem("data")
+console.log(getTaskArr)
 
 
 15.
