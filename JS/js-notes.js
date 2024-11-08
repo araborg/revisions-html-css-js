c390d9c1e9215aa6801259def23ea8f0c791a925
arabog <arababs2015@gmail.com>
2024-11-08 11:15:52 +0100

    updated: Todo List 38

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -472,6 +472,16 @@ localStorage.setItem("data", myTaskArr);
 
 
 13.
+If you check the "Application" tab of your browser console, 
+you'll notice a series of [object Object]. This is because 
+everything you save in localStorage needs to be in string 
+format.
+
+To resolve the issue, wrap the data you're saving in the 
+JSON.stringify() method. Then, check local storage again 
+to observe the results.
+
+localStorage.setItem("data", JSON.stringify(myTaskArr));
 
 
 14.
