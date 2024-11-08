891cc80bd3e724bb11ac535fb8eeaebb0655e33e
arabog <arababs2015@gmail.com>
2024-11-08 11:08:12 +0100

    updated: Todo List 35

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -28,6 +28,11 @@ closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues =
         titleInput.value || dateInput.value || descriptionInput.value;
 
+    const formInputValuesUpdated =
+        titleInput.value !== currentTask.title ||
+        dateInput.value !== currentTask.date ||
+        descriptionInput.value !== currentTask.description;
+
     if (formInputsContainValues) {
         confirmCloseDialog.showModal();
     } else {
