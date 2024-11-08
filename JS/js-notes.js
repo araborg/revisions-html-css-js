b4d3b63188f8fddb4ca4248d28091352d0e3ce79
arabog <arababs2015@gmail.com>
2024-11-08 11:15:37 +0100

    updated: Todo List 37

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -438,7 +438,37 @@ number of items to remove, and the third is an optional
 replacement element. 
 
 
-12.
+12. localStorage:
+const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage offers methods for saving, retrieving, and 
+deleting items. The items you save can be of any JavaScript 
+data type.
+
+For instance, the setItem() method is used to save an item, 
+and the getItem() method retrieves the item. To delete a 
+specific item, you can utilize the removeItem() method, or 
+if you want to delete all items in the storage, you can use 
+clear().
+
+Here's how you can save an item:
+
+Example Code
+localStorage.setItem("key", value); // value could be string, 
+number, or any other data type.
+
+A myTaskArr array has been provided for you. Use the setItem() 
+method to save it with a key of data.
+
+After that, open your browser console and go to the 
+Applications tab, select Local Storage, and the freeCodeCamp 
+domain you see.
+
+localStorage.setItem("data", myTaskArr);
 
 
 13.
--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-tutorial/script.js
@@ -225,3 +225,10 @@ const editTask = (buttonEl) => {
 //
 //
 // stops at 10: review 5- 10 again
+const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("modak", JSON.stringify(myTaskArr));
