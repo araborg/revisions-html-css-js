2bb441c87f94e3d133093739f59bedd9615efc69
arabog <arababs2015@gmail.com>
2024-11-10 16:43:52 +0100

    Todo List: Practice 87

--- revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-practise/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/todo-list-practise/script.js
@@ -24,6 +24,8 @@ const reset = () => {
     titleInput.value = "";
     dateInput.value = "";
     descriptionInput.value = "";
+
+    taskForm.classList.toggle("hidden");
 };
 
 const addOrUpdateTask = () => {
