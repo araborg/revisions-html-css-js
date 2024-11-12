ad0f9bb9bec00e273ab60ed7881e15bc88e09729
arabog <arababs2015@gmail.com>
2024-11-12 09:56:12 +0100

    Todo List: Practice 168

--- revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
@@ -512,8 +512,8 @@ const drugData = [
         description:
             "Practice updating the UI in multiple places based on user actions. The starter download also includes a JSON file to help you practice populating the DOM dynamically.",
 
-        price: "",
-        Qty: "",
+        price: "5400",
+        Qty: "856",
     },
 
     {
