7dba8f9482bbe4dc76cc083fa333f6e1da8b0596
arabog <arababs2015@gmail.com>
2024-11-12 09:55:35 +0100

    Todo List: Practice 166

--- revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
@@ -490,8 +490,8 @@ const drugData = [
         description:
             "Building accessible forms is a crucial task for front-end developers. This challenge will help you practice building a form with several input types and validation.",
 
-        price: "",
-        Qty: "",
+        price: "1300",
+        Qty: "500",
     },
 
     {
