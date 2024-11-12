127e3a7ac229fd71af5e024922cfea7fd7023a9a
arabog <arababs2015@gmail.com>
2024-11-12 09:16:48 +0100

    Todo List: Practice 137

---
+++ revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
@@ -0,0 +1,401 @@
+const drugData = [
+    {
+        id: "galleria-slideshow-site-1731398793077",
+        title: "Galleria slideshow site",
+        date: "2024-11-18",
+        description:
+            "This art gallery slideshow project will be a great test for your layout and JS skills With the masonry layout slideshow logic and lightbox view theres a lot to build",
+    },
+
+    {
+        id: "order-summary-component-1731398773561",
+        title: "Order summary component",
+        date: "2024-11-21",
+        description:
+            "A perfect project for newbies who are starting to build confidence with layouts",
+    },
+
+    {
+        id: "github-user-search-app-1731398751372",
+        title: "GitHub user search app",
+        date: "2024-11-22",
+        description:
+            "In this project youll use the GitHub users API to pull profile data and display it Its a great challenge if youre looking to practice working with a 3rd-party API",
+    },
+
+    {
+        id: "time-tracking-dashboard-1731398732388",
+        title: "Time tracking dashboard",
+        date: "2024-11-21",
+        description:
+            "A perfect opportunity to practice your CSS Grid skills For anyone wanting to take it up a notch we provide a JSON data file to practice working with data",
+    },
+
+    {
+        id: "memory-game-1731398712758",
+        title: "Memory game",
+        date: "2024-11-20",
+        description:
+            "This project will be an excellent test for your HTML CSS and JS skills Youll be building a fun memory game where players try to match pairs",
+    },
+
+    {
+        id: "e-commerce-product-page-1731398695219",
+        title: "E-commerce product page",
+        date: "2024-11-21",
+        description:
+            "In this challenge youll build a beautiful product page Well be putting your JS skills to the test with a lightbox product gallery and cart functionality",
+    },
+
+    {
+        id: "space-tourism-multi-page-website-1731398536411",
+        title: "Space tourism multi-page website",
+        date: "2024-11-20",
+        description:
+            "This project is a fun collaboration with Scrimba and Kevin Powell If you like you can follow along and watch Kevin complete the project on Scrimba Lots to learn",
+    },
+
+    {
+        id: "equalizer-landing-page-1731398336641",
+        title: "Equalizer landing page",
+        date: "2024-11-20",
+        description:
+            "This challenge will be a perfect test of your HTML  CSS skills If youre starting to get comfortable building smaller projects this will be a great step up",
+    },
+
+    {
+        id: "nft-preview-card-component-1731398152531",
+        title: "NFT preview card component",
+        date: "2024-11-20",
+        description:
+            "This HTML  CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with",
+    },
+
+    {
+        id: "art-gallery-website-1731398139847",
+        title: "Art gallery website",
+        date: "2024-11-19",
+        description:
+            "This two-page art gallery project will be perfect if youre ready to branch out beyond single landing pages There are some particularly interesting layout challenges",
+    },
+
+    {
+        id: "interactive-comments-section-1731398122056",
+        title: "Interactive comments section",
+        date: "2024-11-18",
+        description:
+            "This is project will put your JavaScript skills to the test We provide a JSON file to pull the data but its also a perfect project to build as a full-stack CRUD app",
+    },
+
+    {
+        id: "tic-tac-toe-game-1731398105280",
+        title: "Tic Tac Toe game",
+        date: "2024-11-18",
+        description:
+            "This classic practice project for web developers is a great exercise to complete It will test your HTML semantics your grid layout skill as well as your JS logic",
+    },
+
+    {
+        id: "qr-code-component-1731398085492",
+        title: "QR code component",
+        date: "2024-11-17",
+        description:
+            "A perfect first challenge if youre new to HTML and CSS The card layout doesnt shift so its ideal if you havent learned about building responsive layouts yet",
+    },
+
+    {
+        id: "suite-landing-page-1731398066293",
+        title: "Suite landing page",
+        date: "2024-11-16",
+        description:
+            "This small-ish HTML and CSS only landing page includes some interesting layout decisions Perfect if you want to put your layout and responsive skills to the test",
+    },
+
+    {
+        id: "advice-generator-app-1731398042462",
+        title: "Advice generator app",
+        date: "2024-11-16",
+        description:
+            "The perfect project if youre learning how to interact with 3rd-party APIs This challenge uses the Advice Slip API to generate random quotes of advice",
+    },
+
+    {
+        id: "entertainment-web-app-1731398016878",
+        title: "Entertainment web app",
+        date: "2024-11-15",
+        description:
+            "This multi-page entertainment web app will have you working with JSON data routing state management and search functionality",
+    },
+
+    {
+        id: "interactive-rating-component-1731397998077",
+        title: "Interactive rating component",
+        date: "2024-11-16",
+        description:
+            "This is a nice small project to practice handling user interactions and updating the DOM Perfect for anyone who has learned the basics of JavaScript",
+    },
+
+    {
+        id: "in-browser-markdown-editor-1731397982425",
+        title: "In-browser markdown editor",
+        date: "2024-11-16",
+        description:
+            "In this project youll build a fully-functional in-browser markdown editor application with a lightdark mode toggle Its also perfect to build as a full-stack app",
+    },
+
+    {
+        id: "intro-section-with-dropdown-navigation-1731397964106",
+        title: "Intro section with dropdown navigation",
+        date: "2024-11-13",
+        description:
+            "This challenge will test your ability to create dropdown navigation menus a common pattern on larger sites It will also provide some nice basic layout challenges",
+    },
+
+    {
+        id: "skilled-e-learning-landing-page-1731397948035",
+        title: "Skilled e-learning landing page",
+        date: "2024-11-13",
+        description:
+            "This responsive page is perfect if youve learned the fundamentals of HTML  CSS completed a couple of smaller projects and want to take it up a notch",
+    },
+
+    {
+        id: "expenses-chart-component-1731397926788",
+        title: "Expenses chart component",
+        date: "2024-11-14",
+        description:
+            "In this challenge youll create a bar chart component from scratch We provide a local JSON file so you can add the chart data dynamically if you choose",
+    },
+
+    {
+        id: "kanban-task-management-web-app-1731397901468",
+        title: "Kanban task management web app",
+        date: "2024-11-13",
+        description:
+            "In this project youll build a fully-functional task management app with a lightdark mode toggle We provide a JSON file so you can practice working with JSON data",
+    },
+
+    {
+        id: "product-preview-card-component-1731397375898",
+        title: "Product preview card component",
+        date: "2024-11-14",
+        description:
+            "This HTML  CSS-only challenge will be perfect for anyone starting to build responsive projects",
+    },
+
+    {
+        id: "single-page-design-portfolio-1731397348019",
+        title: "Single-page design portfolio",
+        date: "2024-11-14",
+        description:
+            "This project will test your layout skills especially in making the site fully responsive The skills grid is a perfect chance to practice CSS Grid",
+    },
+
+    {
+        id: "interactive-card-details-form-1731397323643",
+        title: "Interactive card details form",
+        date: "2024-11-13",
+        description:
+            "This fun project will be an excellent way to practice DOM manipulation and form validation while also putting your HTML and CSS skills to the test",
+    },
+
+    {
+        id: "password-generator-app-1731397309623",
+        title: "Password generator app",
+        date: "2024-11-13",
+        description:
+            "This app will be an excellent test of your HTML CSS and JS skills Youll build custom form controls and use JavaScript to generate random passwords",
+    },
+
+    {
+        id: "notifications-page-1731397282256",
+        title: "Notifications page",
+        date: "2024-11-13",
+        description:
+            "This project will be a brilliant test of your HTML CSS and basic JavaScript skills Youll use JS to toggle the visual state of the notifications",
+    },
+
+    {
+        id: "connect-four-game-1731397267144",
+        title: "Connect Four game",
+        date: "2024-11-13",
+        description:
+            "Your JS knowledge will be challenged in this fun project where youll build a two-player game Youll write the logic for the game and how to test for win conditions",
+    },
+
+    {
+        id: "news-homepage-1731397026138",
+        title: "News homepage",
+        date: "2024-11-13",
+        description:
+            "This news homepage will be an excellent opportunity to practice your CSS Grid skills There will be lots of tricky decisions to make and plenty of learning opportunities",
+    },
+
+    {
+        id: "single-page-developer-portfolio-1731397002170",
+        title: "Single-page developer portfolio",
+        date: "2024-11-13",
+        description:
+            "Test your HTML and CSS skills with this portfolio site Basic JS knowledge is needed for the form validation You can even customize and use this as your own site",
+    },
+
+    {
+        id: "multi-step-form-1731396985306",
+        title: "Multi-step form",
+        date: "2024-11-13",
+        description:
+            "An excellent test for your form-building and JS skills this project will pose many challenges along the way to completion",
+    },
+
+    {
+        id: "dictionary-web-app-1731396970659",
+        title: "Dictionary web app",
+        date: "2024-11-13",
+        description:
+            "In this project youll integrate with the Dictionary API to create a real-world dictionary web app Additional tests include colour themes and font selection",
+    },
+
+    {
+        id: "results-summary-component-1731396947515",
+        title: "Results summary component",
+        date: "2024-11-12",
+        description:
+            "This challenge has something for everyone Its a HTML and CSS only project but weve also provided a JSON file of the test results for anyone wanting to practice JS",
+    },
+
+    {
+        id: "workit-landing-page-1731396931991",
+        title: "Workit landing page",
+        date: "2024-11-12",
+        description:
+            "This HTML and CSS-only landing page will be an excellent test of your UI and responsive skills Theres also some fun to be had creating the curved section borders",
+    },
+
+    {
+        id: "age-calculator-app-1731396914791",
+        title: "Age calculator app",
+        date: "2024-11-12",
+        description:
+            "This challenge is designed to sharpen your JavaScript and form validation skills Working with dates in JavaScript can be tricky so this will be a nice test",
+    },
+
+    {
+        id: "body-mass-index-calculator-1731396899121",
+        title: "Body Mass Index calculator",
+        date: "2024-11-12",
+        description:
+            "This body mass index BMI tool will be fun to build testing your layout JS and HTML form skills The responsive layout shifts will also be an interesting challenge",
+    },
+
+    {
+        id: "newsletter-sign-up-form-with-success-message-1731396881840",
+        title: "Newsletter sign-up form with success message",
+        date: "2024-11-12",
+        description:
+            "This will test your skills with basic form structure validation and submission The success state will also be an excellent opportunity to work with DOM manipulation",
+    },
+
+    {
+        id: "link-sharing-app-1731396856977",
+        title: "Link-sharing app",
+        date: "2024-11-12",
+        description:
+            "In this project youll build a fully-functional link-sharing app for developers Youll practice working with image uploads repeater fields drag-and-drop and more",
+    },
+
+    {
+        id: "faq-accordion-1731396839241",
+        title: "FAQ accordion",
+        date: "2024-11-12",
+        description:
+            "In this challenge youll build an FAQ accordion This is an extremely common front-end pattern so its an excellent opportunity to get some practice in",
+    },
+
+    {
+        id: "blog-preview-card-1731396820022",
+        title: "Blog preview card",
+        date: "2024-11-12",
+        description:
+            "This HTML  CSS-only challenge is a perfect project for beginners getting up to speed with HTML and CSS fundamentals like HTML structure and the box model",
+    },
+
+    {
+        id: "social-links-profile-1731396772125",
+        title: "Social links profile",
+        date: "2024-11-12",
+        description:
+            "In this small project youll build out your social link-sharing profile You can even personalize it and use it to share all your social profiles",
+    },
+
+    {
+        id: "recipe-page-1731396750952",
+        title: "Recipe page",
+        date: "2024-11-12",
+        description:
+            "This challenge will help you focus on writing semantic HTML Ensure you think through what HTML elements are most appropriate for each piece of content",
+    },
+
+    {
+        id: "frontend-quiz-app-1731351289740",
+        title: "Frontend Quiz app",
+        date: "2024-11-11",
+        description:
+            "This app will test your skills as well as your knowledge as you build out a fully functional quiz We provide a local JSON file to help you practice working with JSON",
+    },
+
+    {
+        id: "hangman-game-1731254969677",
+        title: "Hangman game",
+        date: "2024-11-10",
+        description:
+            "This project is a great all-around test. You'll need good HTML to ensure accessible navigation. There are many tricky CSS details, and the game logic will test your JS.",
+    },
+
+    {
+        id: "contact-forms-1731254870963",
+        title: "Contact forms",
+        date: "2024-11-10",
+        description:
+            "Building accessible forms is a crucial task for front-end developers. This challenge will help you practice building a form with several input types and validation.",
+    },
+
+    {
+        id: "mortgage-repayment-calculators-1731254836032",
+        title: "Mortgage repayment calculators",
+        date: "2024-11-10",
+        description:
+            "This mortgage calculator is an excellent project for practicing working with forms, client-side validation, and updating the DOM. Remember to focus on accessibility, too!",
+    },
+
+    {
+        id: "product-list-with-cart-1731252183347",
+        title: "Product list with cart",
+        date: "2024-11-10",
+        description:
+            "Practice updating the UI in multiple places based on user actions. The starter download also includes a JSON file to help you practice populating the DOM dynamically.",
+    },
+
+    {
+        id: "personal-finance-app-1731252154010",
+        title: "Personal finance app",
+        date: "2024-11-10",
+        description:
+            "This app contains 5 pages (Overview, Transactions, Budgets, Pots, and Recurring Bills) and includes a lot of tricky elements. You can also build it as a full-stack app!",
+    },
+
+    {
+        id: "bento-grid-1731252130634",
+        title: "Bento grid",
+        date: "2024-11-10",
+        description:
+            "This challenge is perfect for testing your CSS Grid and responsive skills with this bento grid layout.",
+    },
+
+    {
+        id: "note-taking-web-app-1731252107870",
+        title: "Note-taking web app",
+        date: "2024-11-10",
+        description:
+            "This project has many fun and challenging features to add, including color and font themes. It can also be built as a full-stack web app!",
+    },
+];
