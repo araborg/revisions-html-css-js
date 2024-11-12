b4633718716622ee27d960c472fc69e58970b449
arabog <arababs2015@gmail.com>
2024-11-12 09:55:19 +0100

    Todo List: Practice 165

--- revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
+++ revision-25-4-24/js-practice/courses/course2/todo-list/kunleara/script.js
@@ -479,8 +479,8 @@ const drugData = [
         description:
             "This project is a great all-around test. You'll need good HTML to ensure accessible navigation. There are many tricky CSS details, and the game logic will test your JS.",
 
-        price: "",
-        Qty: "",
+        price: "3200",
+        Qty: "470",
     },
 
     {
