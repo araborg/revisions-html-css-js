613c50802fbfb3baea9ca82049b3e4553ab3ad10
arabog <arababs2015@gmail.com>
2024-11-18 22:44:56 +0100

    Recursion 75

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -602,7 +602,15 @@ numberInput.addEventListener("keydown", () => {});
 In d above case, d eventlistener was added to d input elemt.
 
 
-3.
+3. event object
+Whenever an event listener is triggered by an event, an event 
+object is created automatically. You don't always need to use 
+this object, like with your click handler above, but it can be 
+useful to access information about the event that was triggered.
+
+First, pass e as a parameter to your callback function. Remember 
+that e is a common parameter name for the event object. Next, 
+log e to the console in the body of your callback function.
 
 
 4.
