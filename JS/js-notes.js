3e82e90058fc42ec5efc751aa8d7097275a89a4b
arabog <arababs2015@gmail.com>
2024-11-18 22:25:33 +0100

    Recursion 64

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -585,7 +585,11 @@ one end, use trimStart() or trimEnd().
 
 
 Recursion:
-1.
+1. Recursion is a programming concept where a function 
+calls itself. 
+
+This can reduce a complex problem into simpler sub-problems,
+until they become straightforward to solve.
 
 
 2.
