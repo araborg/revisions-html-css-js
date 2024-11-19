289e29033c98654e832805d61f6d211bc4b61558
arabog <arababs2015@gmail.com>
2024-11-19 12:02:41 +0100

    Recursion 167

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -794,6 +794,7 @@ A recursive function is a function that calls itself over
 and over. But you have to be careful because you can easily 
 create an infinite loop. 
 
+base case:
 That's where the base case comes in. The base case is when 
 the function stops calling itself, and it is a good idea 
 to write it first.
@@ -802,6 +803,10 @@ Since your countdown() function will count down from a given
 number to zero, the base case is when the number parameter is 
 equal to 0. Then it should return to break out of its 
 recursive loop.
+
+recursive case:
+Recursive functions also have a recursive case, which is where 
+the function calls itself.
 
 
 16.
