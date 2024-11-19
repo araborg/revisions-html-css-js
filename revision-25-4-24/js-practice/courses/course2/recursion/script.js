8e93da9c8d0513380d67b8d3e96ee1de8ef90910
arabog <arababs2015@gmail.com>
2024-11-19 23:19:03 +0100

    Recursion 221

--- revision-25-4-24/js-practice/courses/course2/recursion/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/script.js
@@ -21,6 +21,8 @@ const checkUserInput = () => {
 const decimatToBinary = (input) => {
     if (input === 0) {
         return "0";
+    } else if (input === 1) {
+        return "1";
     } else {
         return decimatToBinary(Math.floor(input / 2)) + (input % 2);
     }
