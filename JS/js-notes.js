69184ab1eb7ae5df0ccc6818434b65a3ea114104
arabog <arababs2015@gmail.com>
2024-11-19 11:36:30 +0100

    Recursion 150

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -773,6 +773,16 @@ console.log(a()); // freeCodeCamp is awesome!
 
 
 14.
+In computer science, a stack is a data structure where items 
+are stored in a LIFO (last-in-first-out) manner. If you imagine 
+a stack of books, the last book you add to the stack is the first 
+book you can take off the stack. Or an array where you can only 
+.push() and .pop() elements.
+
+The call stack is a collection of function calls stored in a 
+stack structure. When you call a function, it is added to the 
+top of the stack, and when it returns, it is removed from 
+the top / end of the stack.
 
 
 15.
