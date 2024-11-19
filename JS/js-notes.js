3ef8b6fb787c79f44e2257ffbafbb3a4bcf05c49
arabog <arababs2015@gmail.com>
2024-11-19 08:05:21 +0100

    Recursion 100

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -668,7 +668,14 @@ Note that alert() is a method on the window object in the browser,
 so you can use either window.alert() or alert().
 
 
-8.
+8. Binary numbers:
+Binary numbers are a base-2 number system. Unlike the base-10 or 
+decimal number system we use every day that uses 10 digits (0-9) 
+to form numbers, the binary number system only has two digits, 
+0 and 1. In computer science, these binary digits are called bits, 
+and are the smallest unit of data computers can process. For 
+computers, 0 represents false or "off", and 1 represents true or 
+"on".
 
 
 9.
