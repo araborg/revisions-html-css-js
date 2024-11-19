0ccc14d1992041c7a6b87f2f6e3ba37f58514985
arabog <arababs2015@gmail.com>
2024-11-19 23:48:17 +0100

    Recursion 230

--- revision-25-4-24/js-practice/courses/course2/recursion/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/script.js
@@ -3,10 +3,6 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-    if (parseInt(numberInput.value) === 5) {
-        console.log("true");
-    }
-
     if (
         !numberInput.value ||
         isNaN(parseInt(numberInput.value)) ||
@@ -15,6 +11,10 @@ const checkUserInput = () => {
         alert("Please provide a decimal number greater than or equal to 0");
 
         return;
+    }
+
+    if (parseInt(numberInput.value) === 5) {
+        console.log("true");
     }
 
     result.textContent = decimatToBinary(parseInt(numberInput.value));
