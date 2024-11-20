0ed934afc3e19028b7d71c905dafd44ef12e8b25
arabog <arababs2015@gmail.com>
2024-11-20 11:18:35 +0100

    Recursion 245

--- revision-25-4-24/js-practice/courses/course2/recursion/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/script.js
@@ -122,7 +122,7 @@ const decimatToBinary = (input) => {
 
     1st time: input = 6
     while (input > 0) {
-                    6 % 2 = 0 + ""
+                    6 % 2 = 0 + "" i.e binary = "0"
         binary = (input % 2) + binary;
 
         input = Math.floor(input / 2);
