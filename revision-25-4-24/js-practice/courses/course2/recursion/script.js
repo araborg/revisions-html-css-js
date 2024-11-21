6e59d93273adfe5c6fa3525ea1f0c620c4afb227
arabog <arababs2015@gmail.com>
2024-11-21 09:47:53 +0100

    Recursion 312

--- revision-25-4-24/js-practice/courses/course2/recursion/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/script.js
@@ -50,6 +50,12 @@ const animationData = [
         marginTop: -200,
         addElDelay: 1500,
     },
+
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000,
+    },
 ];
 
 // event listeners:
