8733c10f6a108cdfc0ca91f0e66c597296422494
arabog <arababs2015@gmail.com>
2024-12-02 11:01:30 +0100

    Recursion review: 63

---
+++ revision-25-4-24/js-practice/courses/course2/recursion/recursn-3/script.js
@@ -0,0 +1,5 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const outputNumber = document.getElementById("output-number");
+
+const animationStack = document.getElementById("animation-stack");
