34e75d03dae70077cf14d8646f1ed4550f729f6f
arabog <arababs2015@gmail.com>
2024-12-13 08:27:01 +0100

    Recursion review: 59

--- revision-25-4-24/js-practice/courses/course2/recursion/recursn-3/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/recursn-3/script.js
@@ -83,6 +83,7 @@ const getInput = (input) => {
     } else if (input === 1) {
         return 1;
     } else {
+        console.log(input % 2);
         getInput(parseInt(Math.floor(input / 2))) + (input % 2);
     }
 };
