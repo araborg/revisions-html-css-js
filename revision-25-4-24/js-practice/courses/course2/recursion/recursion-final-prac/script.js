595040be7bd1e3e84aec3a9f8ec36220d123da63
arabog <arababs2015@gmail.com>
2025-01-02 11:10:58 +0100

    Recursion final review: 17

--- revision-25-4-24/js-practice/courses/course2/recursion/recursion-final-prac/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/recursion-final-prac/script.js
@@ -30,7 +30,7 @@ const animationData = [
 const getInputVal = () => {
     const inputVal = parseInt(numberInput.value);
 
-    if (!numberInput.value || inputVal < 0) {
+    if (!numberInput.value || inputVal < 0 || isNaN(inputVal)) {
         alert("Please provide a decimal number greater than or equal to 0");
     }
 
