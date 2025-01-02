d99d80214805c9a124cbfba012be7f27e6eb2523
arabog <arababs2015@gmail.com>
2025-01-02 11:21:25 +0100

    Recursion final review: 24

--- revision-25-4-24/js-practice/courses/course2/recursion/recursion-final-prac/script.js
+++ revision-25-4-24/js-practice/courses/course2/recursion/recursion-final-prac/script.js
@@ -2,7 +2,6 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 
 const result = document.getElementById("result");
-console.log(result);
 
 const animationData = [
     {
