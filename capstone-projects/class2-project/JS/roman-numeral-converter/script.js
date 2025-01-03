d208a823fc471529e96fbb0ff8aad0f9de71c1d6
arabog <arababs2015@gmail.com>
2025-01-03 09:45:14 +0100

    Roman Numeral Capstone Project: 32

--- capstone-projects/class2-project/JS/roman-numeral-converter/script.js
+++ capstone-projects/class2-project/JS/roman-numeral-converter/script.js
@@ -18,32 +18,30 @@ const getInputVal = () => {
 
 const convertToRoman = (input) => {
     const romans = [
-        
-
-        ["M",	1000],
-['CM'	900],
-['D'	500],
-['CD'	400].
-['C'	100],
-['XC'	90],
-["L"	50],
-['XL'	40],
-['X'	10],
-['IX'	9],
-['V'	5],
-['IV'	4],
-['I'	1],
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1],
     ];
 
     const result = [];
 
     romans.forEach((arr) => {
         while (input > arr[1]) {
+            console.log(arr);
+
             result.push(arr[0]);
 
             input -= arr[1];
-
-            console.log(input);
         }
     });
 
