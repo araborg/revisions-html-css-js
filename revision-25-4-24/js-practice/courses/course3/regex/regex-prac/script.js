4e73f11fb56d4732b68e1bd01654b935bec20b07
arabog <arababs2015@gmail.com>
2025-01-03 15:01:38 +0100

    Regular Expression: 7

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -2,7 +2,7 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageBtn = document.getElementById("check-message-btn");
 
-console.log(messageInput, result, checkMessageBtn);
+// console.log(messageInput, result, checkMessageBtn);
 
 checkMessageBtn.addEventListener("click", () => {
     if (!messageInput.value) {
