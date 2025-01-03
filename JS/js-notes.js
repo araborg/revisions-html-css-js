e242f4f89d0815c2d0738451d7902236d13aadc1
arabog <arababs2015@gmail.com>
2025-01-03 04:52:17 +0100

    Roman Numeral Capstone Project: 23

--- JS/js-notes.js
+++ JS/js-notes.js
@@ -937,6 +937,33 @@ stack.
 
 23.
 
+const ref = [
+        ["M", 1000],
+        ["CM", 900],
+        ["D", 500],
+        ["CD", 400],
+        ["C", 100],
+        ["XC", 90],
+        ["L", 50],
+        ["XL", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1],
+    ];
+
+    const res = [];
+
+    ref.forEach((arr) => {
+        while (input >= arr[1]) {
+            res.push(arr[0]);
+
+            input -= arr[1];
+        }
+    });
+
+    return res.join("");
 
 24.
 
--- capstone-projects/class2-project/JS/roman-numeral-converter/script.js
+++ capstone-projects/class2-project/JS/roman-numeral-converter/script.js
@@ -16,35 +16,7 @@ const getInputVal = () => {
     }
 };
 
-const convertToRoman = (input) => {
-    const ref = [
-        ["M", 1000],
-        ["CM", 900],
-        ["D", 500],
-        ["CD", 400],
-        ["C", 100],
-        ["XC", 90],
-        ["L", 50],
-        ["XL", 40],
-        ["X", 10],
-        ["IX", 9],
-        ["V", 5],
-        ["IV", 4],
-        ["I", 1],
-    ];
-
-    const res = [];
-
-    ref.forEach((arr) => {
-        while (input >= arr[1]) {
-            res.push(arr[0]);
-
-            input -= arr[1];
-        }
-    });
-
-    return res.join("");
-};
+const convertToRoman = (input) => {};
 
 convertBtn.addEventListener("click", getInputVal);
 
