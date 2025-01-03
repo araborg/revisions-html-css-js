476f084e462d0b80e03f48612bb8bd94b64f0557
arabog <arababs2015@gmail.com>
2025-01-03 04:12:42 +0100

    Roman Numeral Capstone Project: 12

--- capstone-projects/class2-project/JS/roman-numeral-converter/script.js
+++ capstone-projects/class2-project/JS/roman-numeral-converter/script.js
@@ -41,21 +41,21 @@ const convertToRoman = (input) => {
     //     return "X";
     // }
 
-    if (input === 1) {
-        return "I";
-    } else if (input === 5) {
-        return "V";
-    } else if (input === 10) {
-        return "X";
-    } else if (input === 50) {
-        return "L";
-    } else if (input === 100) {
-        return "C";
-    } else if (input === 500) {
-        return "D";
-    } else if (input === 1000) {
-        return "M";
-    }
+    // if (input === 1) {
+    //     return "I";
+    // } else if (input === 5) {
+    //     return "V";
+    // } else if (input === 10) {
+    //     return "X";
+    // } else if (input === 50) {
+    //     return "L";
+    // } else if (input === 100) {
+    //     return "C";
+    // } else if (input === 500) {
+    //     return "D";
+    // } else if (input === 1000) {
+    //     return "M";
+    // }
 
     const inputs = [];
     const results = [];
