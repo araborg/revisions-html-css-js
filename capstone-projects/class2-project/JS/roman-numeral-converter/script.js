a172aefadaf3bc45f9acd84d43a29951dcb90da2
arabog <arababs2015@gmail.com>
2025-01-03 03:56:22 +0100

    Roman Numeral Capstone Project: 6

--- capstone-projects/class2-project/JS/roman-numeral-converter/script.js
+++ capstone-projects/class2-project/JS/roman-numeral-converter/script.js
@@ -13,10 +13,34 @@ const getInputVal = () => {
         output.textContent = "Please enter a number less than or equal to 3999";
     } else {
         output.textContent = convertToRoman(inputVal);
+
+        console.log(convertToRoman(inputVal));
     }
 };
 
 const convertToRoman = (input) => {
+    // if (input === 1) {
+    //     return "I";
+    // } else if (input === 2) {
+    //     return "II";
+    // } else if (input === 3) {
+    //     return "III";
+    // } else if (input === 4) {
+    //     return "IV";
+    // } else if (input === 5) {
+    //     return "V";
+    // } else if (input === 6) {
+    //     return "VI";
+    // } else if (input === 7) {
+    //     return "VII";
+    // } else if (input === 8) {
+    //     return "VIII";
+    // } else if (input === 9) {
+    //     return "IX";
+    // } else if (input === 10) {
+    //     return "X";
+    // }
+
     if (input === 1) {
         return "I";
     } else if (input === 2) {
@@ -40,10 +64,7 @@ const convertToRoman = (input) => {
     }
 
     if (input >= 1000 && input < 4000) {
-        noOfLoop.loop += 1;
-        // console.log(convertToRoman(input - 1000) + (noOfLoop += 1));
-        convertToRoman(input - 1000);
-        console.log(noOfLoop);
+        // return convertToRoman(input -) + Math.floor(input / 1000);
     }
 };
 
