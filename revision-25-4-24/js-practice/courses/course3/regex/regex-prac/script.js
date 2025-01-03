a0762c00cfa4d067d90f7985061a47a17247e0d5
arabog <arababs2015@gmail.com>
2025-01-03 14:57:55 +0100

    Regular Expression: 4

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -1,3 +1,5 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
-const checkMessageBtn = document.getElementById("checkMessageBtn");
+const checkMessageBtn = document.getElementById("check-message-btn");
+
+console.log(messageInput, result, checkMessageBtn);
