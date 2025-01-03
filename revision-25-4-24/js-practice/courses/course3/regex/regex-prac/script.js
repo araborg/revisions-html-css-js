6ab38ad192ab2f8093a65809b31b6fedd44324a9
arabog <arababs2015@gmail.com>
2025-01-03 15:00:27 +0100

    Regular Expression: 5

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -3,3 +3,5 @@ const result = document.getElementById("result");
 const checkMessageBtn = document.getElementById("check-message-btn");
 
 console.log(messageInput, result, checkMessageBtn);
+
+checkMessageBtn.addEventListener("click", () => {});
