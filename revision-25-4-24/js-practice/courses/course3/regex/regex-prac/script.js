3c82268dbcc1ff5d13f9741f1fd05685083826c0
arabog <arababs2015@gmail.com>
2025-01-04 06:14:57 +0100

    Regular Expression: 46

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -20,8 +20,8 @@ end: $
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 // const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y\s/i;
-const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y\s/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex];
 
