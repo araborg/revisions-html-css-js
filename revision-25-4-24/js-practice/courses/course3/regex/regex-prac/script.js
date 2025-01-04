150e7a6c98c47456050241476bd869784752b63c
arabog <arababs2015@gmail.com>
2025-01-04 06:26:19 +0100

    Regular Expression: 55

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -19,12 +19,18 @@ end: $
 */
 
 const helpRegex = /please help|assist me/i;
+
 const dollarRegex = //
     /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+
 const freeRegex = //
     /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
-const stockRegex = /(?:\s|^)[s5][t7][0o][c{[(]k [a@4]l[e3]rt(?:\s|$)/i;
-const dearRegex = /dear friend/i;
+
+const stockRegex = //
+    /(?:\s|^)[s5][t7][0o][c{[(]k [a@4]l[e3]rt(?:\s|$)/i;
+
+const dearRegex = //
+    /(?:^|\s)d[e3][a@4]r fr[i1][e3]nd/i;
 
 const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
