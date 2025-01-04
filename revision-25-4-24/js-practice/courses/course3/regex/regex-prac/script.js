95c07464c2df077f1b7d4b8ac5905d62ead106e7
arabog <arababs2015@gmail.com>
2025-01-04 05:56:09 +0100

    Regular Expression: 32

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -16,7 +16,7 @@ non-capturing group: ?:
 
 const helpRegex = /please help|assist me/i;
 const dollarRegex =
-    /[0-9]+ (hundred|thousand|million|billion)? dollars|[0-9]+dollars/i;
+    /[0-9]+ (?:hundred|thousand|million|billion)? dollars|[0-9]+dollars/i;
 
 const denyList = [helpRegex, dollarRegex];
 
