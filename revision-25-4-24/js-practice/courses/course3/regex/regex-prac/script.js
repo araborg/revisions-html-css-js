0aa9dce8ae9ad796fc5938b7e0039e0c154619dd
arabog <arababs2015@gmail.com>
2025-01-04 05:42:59 +0100

    Regular Expression: 26

--- revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regex-prac/script.js
@@ -12,7 +12,7 @@ quantifier: +
 */
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex = /[0-9] dollars|[0-9]dollars/i;
+const dollarRegex = /[0-9]+ dollars|[0-9]+dollars/i;
 
 const denyList = [helpRegex, dollarRegex];
 
