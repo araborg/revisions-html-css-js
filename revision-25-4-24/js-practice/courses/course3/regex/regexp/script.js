e51990edfa32e1b9f27e8260a87239827a494490
arabog <arababs2015@gmail.com>
2025-01-08 11:37:54 +0100

    regex revision updated 42

--- revision-25-4-24/js-practice/courses/course3/regex/regexp/script.js
+++ revision-25-4-24/js-practice/courses/course3/regex/regexp/script.js
@@ -3,7 +3,7 @@ const checkBtn = document.getElementById("check-message-btn");
 const result = document.getElementById("result");
 
 const helpRegex = /pl[e3][a4][s5][e3] h[e3][l1]p|assist me/;
-const dollarRegex = /[0-9]+ hundred|thousand|million|billion? dollars/;
+const dollarRegex = /[0-9]+ (hundred|thousand|million|billion)? dollars/;
 
 const denyList = [helpRegex, dollarRegex];
 
