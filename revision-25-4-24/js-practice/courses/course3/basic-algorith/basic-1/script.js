301a6c19d4dcd76303c4dfedb8827e1f48422dc5
arabog <arababs2015@gmail.com>
2025-01-10 16:28:45 +0100

    Sort algorithm revision 99

--- revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-1/script.js
@@ -2,6 +2,10 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
+
+    const inputValues = document.getElementsByClassName("values-dropdown");
+
+    console.log(inputValues);
 };
 
 sortButton.addEventListener("click", sortInputArray);
