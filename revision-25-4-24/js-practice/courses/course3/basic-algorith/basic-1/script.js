ad5b372aa0db876cf76630dfa889d3ca80bd21a9
arabog <arababs2015@gmail.com>
2025-01-11 06:14:24 +0100

    Sort algorithm revision 114

--- revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-1/script.js
@@ -23,8 +23,6 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
-            console.log(array[j], array[j + 1]);
-
             if (array[j] > array[j + 1]) {
                 const temp = array[j];
 
