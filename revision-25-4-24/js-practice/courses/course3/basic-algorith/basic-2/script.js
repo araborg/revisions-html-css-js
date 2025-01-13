a6c80f407e0123cf7652740132be9452a89683e8
arabog <arababs2015@gmail.com>
2025-01-13 13:49:00 +0100

    sort algorithm revision 30

--- revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-2/script.js
+++ revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-2/script.js
@@ -62,22 +62,5 @@ const bubbleSort = (array) => {
 };
 
 // Selection sort algorithm
-const selectionSort = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        let minIndex = i;
-
-        for (let j = 1; j < array.length; j++) {
-            if (array[j] < array[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        const temp = array[minIndex];
-        array[minIndex] = array[i];
-        array[i] = temp;
-    }
-
-    return array;
-};
 
 // Insertion sort algorithm
