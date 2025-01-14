b9dc4faf46301cebbc4b59fc2c18fed126058cfd
arabog <arababs2015@gmail.com>
2025-01-14 16:10:46 +0100

    Advanced array methods: revision 9

--- revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
@@ -2,7 +2,9 @@ const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
 
-    const numbers = array.map();
+    const numbers = array.map((el) => Number(el));
+
+    const filtered = numbers.filter();
 
     console.log(value);
 };
