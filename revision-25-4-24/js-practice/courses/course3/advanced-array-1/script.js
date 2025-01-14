ce6a69b88ed217a8d8f15bbbc0a3608b550c6e9a
arabog <arababs2015@gmail.com>
2025-01-14 16:19:28 +0100

    Advanced array methods: revision 12

--- revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
@@ -2,9 +2,10 @@ const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
 
-    const numbers = array.map((el) => Number(el));
-
-    const filtered = numbers.filter();
+    const numbers = array
+        .map((el) => Number(el))
+        .filter((el) => !isNaN(el) !== NaN);
+    console.log(numbers);
 };
 
 console.log(NaN === NaN);
