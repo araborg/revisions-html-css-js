6607e4f5b91bba0c345c284ad09d8527f7423369
arabog <arababs2015@gmail.com>
2025-01-15 15:26:46 +0100

    Advanced arr: practice 13

--- revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-3/script.js
+++ revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-3/script.js
@@ -21,7 +21,10 @@ const updateUI = (array) => {
 // bubble sort
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length - 1; j++) {}
+        for (let j = i + 1; j < array.length - 1; j++) {
+            if (array[j] > array[i]) {
+            }
+        }
     }
 };
 
