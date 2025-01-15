ef3916fb266cfd49f450de11ef3878598e7cd29a
arabog <arababs2015@gmail.com>
2025-01-15 12:30:30 +0100

    Advanced array methods: Review 53

--- revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/advanced-array-1/script.js
@@ -12,6 +12,7 @@ const calculate = () => {
     const mode = getMode(numbers);
     const range = getRange(numbers);
     const variance = getVariance(numbers);
+    const standardDeviation = getStandardDeviation(numbers);
 
     document.querySelector("#mean").textContent = mean;
     document.querySelector("#median").textContent = median;
