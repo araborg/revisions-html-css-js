e872f959a18486bceb7bd737b2d4672f64d1df7c
arabog <arababs2015@gmail.com>
2025-01-15 15:06:28 +0100

    Advanced arr: practice 7

--- revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-3/script.js
+++ revision-25-4-24/js-practice/courses/course3/basic-algorith/basic-3/script.js
@@ -3,10 +3,7 @@ const sortBtn = document.getElementById("sort");
 sortBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    console.log(e);
-
     const values = document.getElementsByClassName("values-dropdown");
-
     console.log(values);
 });
 
