93e375e2c2afd9cfb41904c5944bd47843a21c2a
arabog <arababs2015@gmail.com>
2025-01-25 19:07:45 +0100

    Fxnal programming: Review 118

--- revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
@@ -116,12 +116,12 @@ const update = (event) => {
     const value = element.value.replace(/\s/g, "");
 
     if (!value.includes(element.id) && value[0] === "=") {
-        console.log(value);
-
         element.value = evalFormula(
             value.slice(1),
             Array.from(document.getElementById("container").children)
         );
+
+        console.log(element.value);
     }
 };
 
