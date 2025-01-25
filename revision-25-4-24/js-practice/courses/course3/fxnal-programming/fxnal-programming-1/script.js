78edb2b6a081e18be598f59751d6c86fb26ffcc2
arabog <arababs2015@gmail.com>
2025-01-25 13:01:47 +0100

    Fxnal programming: Revisn practise 102

--- revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
@@ -91,6 +91,9 @@ const spreadsheetFunctions = {
     median,
 
     even: (nums) => nums.filter(isEven),
+
+    firsttwo: (nums) => nums.slice(0, 2),
+    lasttwo: (nums) => nums.slice(-2),
 };
 
 const update = (event) => {
