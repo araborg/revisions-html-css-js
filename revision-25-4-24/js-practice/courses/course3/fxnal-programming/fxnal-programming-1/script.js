1e7716790281d062f26570b1f1074e65c6efacf3
arabog <arababs2015@gmail.com>
2025-01-25 11:47:57 +0100

    Fxnal programming: Revisn practise 73

--- revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
@@ -102,12 +102,11 @@ const update = (event) => {
 
 const evalFormula = (x, cells) => {
     // cells is an array
-    const idToText = (id) => {
+    const idToText = (id) =>
         /* cell.id === id returns an input elemt 
             while.value returns value
         */
-        return cells.find((cell) => cell.id === id).value;
-    };
+        cells.find((cell) => cell.id === id).value;
 
     const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
 
@@ -143,5 +142,9 @@ const evalFormula = (x, cells) => {
 
     const cellRegex = /[A-J][1-9][0-9]?/gi;
 
-    const cellExpanded = rangeExpanded.replace(cellRegex, () => {});
+    const cellExpanded =
+        //
+        rangeExpanded.replace(cellRegex, (match) => {
+            idToText(match.toUpperCase());
+        });
 };
