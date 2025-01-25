95ccc7ce2f56e348ac97297751037b62fe5b70af
arabog <arababs2015@gmail.com>
2025-01-25 12:30:45 +0100

    Fxnal programming: Revisn practise 88

--- revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
@@ -172,4 +172,6 @@ const applyFunction = (str) => {
     const infix = /([\d.]+)([+-])([\d.]+)/;
 
     const str2 = infixEval(noHigh, infix);
+
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
 };
