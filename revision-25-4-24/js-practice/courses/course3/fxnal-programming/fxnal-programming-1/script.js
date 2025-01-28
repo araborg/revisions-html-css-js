c14ad20905460815055b3a95b13d37aebe68d46f
arabog <arababs2015@gmail.com>
2025-01-28 19:24:44 +0100

    Fxnal programming: Review 129

--- revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
+++ revision-25-4-24/js-practice/courses/course3/fxnal-programming/fxnal-programming-1/script.js
@@ -167,8 +167,6 @@ const evalFormula = (x, cells) => {
                 .map(addCharacters(char1)(char2))
     );
 
-    console.log(x, rangeRegex, rangeExpanded);
-
     const cellRegex = /[A-J][1-9][0-9]?/gi;
 
     const cellExpanded =
