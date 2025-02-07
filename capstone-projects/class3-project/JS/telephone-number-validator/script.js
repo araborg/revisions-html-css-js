43d8441dd7969399f4f57213eb93742bb89a8005
arabog <arababs2015@gmail.com>
2025-02-07 20:26:04 +0100

    telephone-number-validator 45

--- capstone-projects/class3-project/JS/telephone-number-validator/script.js
+++ capstone-projects/class3-project/JS/telephone-number-validator/script.js
@@ -13,8 +13,6 @@ const removeSpace = (str) => {
 checkBtn.addEventListener("click", () => {
     const parseNum = parseInt(userInput.value);
 
-    console.log(isNaN(parseNum));
-
     if (!userInput.value || isNaN(parseNum)) {
         alert("Please provide a phone number");
     } else {
