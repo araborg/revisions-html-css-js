efeb3e7a7d030c86a00b4d34d9b4455d677cce52
arabog <arababs2015@gmail.com>
2025-02-10 21:22:00 +0100

    telephone-number-validator 145

--- capstone-projects/class3-project/JS/telephone-number-validator/script.js
+++ capstone-projects/class3-project/JS/telephone-number-validator/script.js
@@ -15,7 +15,8 @@ const checkValidNumber = (input) => {
     const phoneNumber = "[0-9]{3}[\\s\\-]?[0-9]{4}$";
 
     const phoneRegex = new RegExp(
-        `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+        `${countryCode}${areaCode}`
+        // `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
     );
 
     console.log(phoneRegex.test(input));
@@ -55,3 +56,42 @@ userInput.addEventListener("keydown", (e) => {
 clearBtn.addEventListener("click", () => {
     resultsDiv.textContent = "";
 });
+
+/*
+    Valid US number:    Length:      Spaces:    Hyphen:            
+    5555555555          // 10           0       0
+    555-555-5555        // 10           0       2
+    1 555-555-5555      // 11           1       2
+    1 555 555 5555      // 11           3       0
+    1 456 789 4444      // 11           3       0
+    (555)555-5555       // 12           0       1
+    1 (555) 555-5555    // 13           2       1
+    1(555)555-5555      // 13           0       1
+    1 (555) 555-5555    // 13           2       1
+
+    factors to be considered:
+    Length
+    
+
+
+    Invalid US number:
+    555-5555            // 7            0       1
+    55555555            // 8            0       0
+    55 55-55-555-5      // 10           1       3
+    2 757 622-7382      // 11           2       1
+    27576227382         // 11           0       0
+    555)-555-5555       // 11           0       2
+    (555-555-5555       // 11           0       2
+    1 555)555-5555      // 12           1       1
+    (6054756961)        // 12           0       0
+    11 555-555-5555     // 12           1       2
+    2 (757) 622-7382    // 13           2       1
+    123**&!!asdf#       // 13           0       0
+    0 (757) 622-7382    // 13           2       1
+    -1 (757) 622-7382   // 13           2       1
+    (275)76227382       // 13           0       0
+    2(757)6227382       // 13           0       0
+    2(757)622-7382      // 13           0       1
+    10 (757) 622-7382   // 14           2       1
+    (555)5(55?)-5555    // 15           0       1
+*/
