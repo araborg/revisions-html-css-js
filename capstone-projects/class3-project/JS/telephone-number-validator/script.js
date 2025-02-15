2fafb7a7020f41478d0427a049588a849c8bd04b
arabog <arababs2015@gmail.com>
2025-02-15 15:35:36 +0100

    telephone-number-validator 169

--- capstone-projects/class3-project/JS/telephone-number-validator/script.js
+++ capstone-projects/class3-project/JS/telephone-number-validator/script.js
@@ -2,14 +2,16 @@
 #00471b
 #4d3800
 
-The format is 
+The format is: 
+(country code): 1
+
 (area code): (555)  
 
 (exchange code): 555
 
 (subscriber number): 1234 
 
-For example, (555) 555-1234. 
+For example,1 (555) 555-1234. 
 
 */
 const userInput = document.getElementById("user-input");
