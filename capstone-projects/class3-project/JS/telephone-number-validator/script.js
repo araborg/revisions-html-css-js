0d9b11e7d87c52e502489601e8e9c29f0ad8c8d7
arabog <arababs2015@gmail.com>
2025-02-18 18:38:10 +0100

    telephone-number-validator 183

--- capstone-projects/class3-project/JS/telephone-number-validator/script.js
+++ capstone-projects/class3-project/JS/telephone-number-validator/script.js
@@ -31,3 +31,5 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
+
+console.log(userInput, checkBtn, clearBtn, result);
