7b5d8ab989a86ae6d2d1e99b4bfe64a38dfb495b
arabog <arababs2015@gmail.com>
2025-02-18 20:04:14 +0100

    telephone-number-validator 232

--- capstone-projects/class3-project/JS/telephone-number-validator/script.js
+++ capstone-projects/class3-project/JS/telephone-number-validator/script.js
@@ -2,29 +2,6 @@
 [] denotes A CHARACTER class
 
 () denotes a capturing GROUP
-
-
-#00471b
-#4d3800
-
-
-The format is: 
-(country code): 1
-(area code): (555)  
-(exchange code): 555
-(subscriber number): 1234 
-
-For example,1 (555) 555-1234. 
-
-
-    1 555-555-5555
-    1 (555) 555-5555
-    1(555)555-5555
-    1 555 555 5555
-    5555555555
-    555-555-5555
-    (555)555-5555
-
 */
 
 const userInput = document.getElementById("user-input");
