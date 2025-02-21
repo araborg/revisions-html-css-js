e4928bd1d8e5c90e210ce12876ca25dbe3d763c5
arabog <arababs2015@gmail.com>
2025-02-21 09:38:17 +0100

    OOP: Shopping cart: 5

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -1,3 +1,5 @@
 const cartContainer = document.getElementById("cart-container");
 const productsContainer = document.getElementById("products-container");
 const dessertCards = document.getElementById("dessert-card-container");
+
+console.log(cartContainer, productsContainer, dessertCards);
