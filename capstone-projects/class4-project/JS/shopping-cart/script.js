6efab2585466235d6a92bd9ef5d6264f526551be
arabog <arababs2015@gmail.com>
2025-02-24 14:12:31 +0100

    OOP: Shopping cart: 15

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -132,5 +132,11 @@ class ShoppingCart {
             totalCountPerProduct[dessert.id] =
                 (totalCountPerProduct[dessert.id] || 0) + 1;
         });
+
+        const currentProductCount = totalCountPerProduct[product.id];
+
+        const currentProductCountSpan = document.getElementById(
+            `product-count-id${product.id}`
+        );
     }
 }
