f56b5728a20017fd4fe8c0af7fe5e4655c80348e
arabog <arababs2015@gmail.com>
2025-02-25 20:54:15 +0100

    OOP: Shopping cart: 30

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -160,4 +160,6 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
     });
 });
 
-cartBtn.addEventListener("click", () => {});
+cartBtn.addEventListener("click", () => {
+    isCartShowing = !isCartShowing;
+});
