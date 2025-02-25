82cdb88b6324667544d23e20e31724293d3628f9
arabog <arababs2015@gmail.com>
2025-02-25 22:03:54 +0100

    OOP: Shopping cart: 33

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -139,6 +139,8 @@ class ShoppingCart {
             `product-count-id${product.id}`
         );
 
+        console.log(currentProductCountSpan);
+
         currentProductCount > 1
             ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
             : (productsContainer.innerHTML += `<div class="product" id="dessert${id}">
