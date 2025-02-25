1845702df79b53b213764d01810b376fa31fb2ea
arabog <arababs2015@gmail.com>
2025-02-25 22:05:43 +0100

    OOP: Shopping cart: 35

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -153,7 +153,7 @@ class ShoppingCart {
 const cart = new ShoppingCart();
 
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
-// console.log(addToCartBtns);
+
 [...addToCartBtns].forEach((btn) => {
     btn.addEventListener("click", (event) => {
         cart.addItem(Number(event.target.id), products);
