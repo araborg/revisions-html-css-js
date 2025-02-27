a9abdada6d4cd11feef86cf6dd95a81aa157fdbe
arabog <arababs2015@gmail.com>
2025-02-27 19:26:41 +0100

    OOP: Shopping cart: 50

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -168,6 +168,10 @@ class ShoppingCart {
         this.total = subTotal + tax;
 
         cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
     }
 
     calculateTaxes(amount) {
