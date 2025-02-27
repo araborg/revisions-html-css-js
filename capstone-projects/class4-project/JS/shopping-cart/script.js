9e697733921662cd6daa17c987446e32f0ac8cfd
arabog <arababs2015@gmail.com>
2025-02-27 19:42:24 +0100

    OOP: Shopping cart: 61

--- capstone-projects/class4-project/JS/shopping-cart/script.js
+++ capstone-projects/class4-project/JS/shopping-cart/script.js
@@ -196,6 +196,11 @@ class ShoppingCart {
             this.total = 0;
 
             productsContainer.innerHTML = ``;
+
+            totalNumberOfItems.textContent = 0;
+            cartSubTotal.textContent = 0;
+            cartTaxes.textContent = 0;
+            cartTotal.textContent = 0;
         }
     }
 }
