455521f79bd94337f22e46012629d270ab5a370c
arabog <arababs2015@gmail.com>
2025-03-03 22:02:16 +0100

    OOP: Platformer Game: 34

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -47,8 +47,11 @@ class Player {
 
         this.position.x += this.velocity.x; // moves right
 
-        if (this.position.y + this.height + this.velocity.y <= canvas.height)
-            this.position.y += this.velocity.y; // jumps up
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            if (this.position.y < 0) {
+            }
+        }
+        this.position.y += this.velocity.y; // jumps up
     }
 }
 
