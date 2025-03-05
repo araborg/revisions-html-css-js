3dfad0332f87f959dbb230e3acb0518af67e0d60
arabog <arababs2015@gmail.com>
2025-03-05 16:36:15 +0100

    OOP: Platformer Game: 58

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -91,7 +91,11 @@ class Player {
 
 const player = new Player();
 
-const startGame = () => {};
+const startGame = () => {
+    canvas.style.display = "block";
+
+    startScreen.style.display = "none";
+};
 //
 
 /*
