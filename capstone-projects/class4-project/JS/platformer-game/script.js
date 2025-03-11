dccf186a5ef94104b76b36643e4f8589c10929b2
arabog <arababs2015@gmail.com>
2025-03-11 04:35:59 +0100

    OOP: Platformer Game: 87

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -159,7 +159,9 @@ window.addEventListener("keydown", ({ key }) => {
     movePlayer(key, 8, true);
 });
 
-window.addEventListener("keyup", ({ key }) => {});
+window.addEventListener("keyup", ({ key }) => {
+    movePlayer(key, 0, false);
+});
 
 /*
 Canvas methods:
