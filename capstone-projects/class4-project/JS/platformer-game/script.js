2bc12d0c57d6d0ebf28ada7aec77e313d3186338
arabog <arababs2015@gmail.com>
2025-03-13 14:46:15 +0100

    Platformer Game: tutorial-script.js: 103

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -130,7 +130,16 @@ const animate = () => {
                 platform.position.y,
 
             player.position.x >= platform.position.x - player.width / 2,
+
+            player.position.x <=
+                platform.position.x + platform.width - player.width / 3,
         ];
+
+        if (collisionDetectionRules.every((rule) => rule)) {
+            player.velocity.y = 0;
+
+            return;
+        }
     });
 };
 
