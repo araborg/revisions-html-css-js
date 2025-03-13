8a438ad2e8373394ba3cb51a5b3edaafea762ea3
arabog <arababs2015@gmail.com>
2025-03-13 18:59:23 +0100

    Platformer Game: tutorial-script.js: 139

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -192,6 +192,9 @@ const animate = () => {
 
         if (checkpointDetectionRules.every((rule) => rule)) {
             checkpoint.claim();
+
+            if (index === checkpoints.length - 1) {
+            }
         }
     });
 };
