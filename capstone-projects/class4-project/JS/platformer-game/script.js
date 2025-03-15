43e073efba6746fa7401ef686ce79ce4beb33180
arabog <arababs2015@gmail.com>
2025-03-15 11:04:21 +0100

    Platformer Game: tutorial-script.js: 216

--- capstone-projects/class4-project/JS/platformer-game/script.js
+++ capstone-projects/class4-project/JS/platformer-game/script.js
@@ -106,6 +106,27 @@ class Platform {
     }
 }
 
+const platformPositions = [
+    { x: 500, y: proportionalSize(450) },
+    { x: 700, y: proportionalSize(400) },
+    { x: 850, y: proportionalSize(350) },
+    { x: 900, y: proportionalSize(350) },
+    { x: 1050, y: proportionalSize(150) },
+
+    { x: 2500, y: proportionalSize(450) },
+    { x: 2900, y: proportionalSize(400) },
+    { x: 3150, y: proportionalSize(350) },
+
+    { x: 3900, y: proportionalSize(450) },
+    { x: 4200, y: proportionalSize(400) },
+    { x: 4400, y: proportionalSize(200) },
+    { x: 4700, y: proportionalSize(150) },
+];
+
+const platforms = platformPositions.map(
+    (platform) => new Platform(platform.x, platform.y)
+);
+
 class CheckPoint {
     constructor(x, y, z) {
         this.position = {
@@ -136,27 +157,6 @@ class CheckPoint {
 }
 
 const player = new Player();
-
-const platformPositions = [
-    { x: 500, y: proportionalSize(450) },
-    { x: 700, y: proportionalSize(400) },
-    { x: 850, y: proportionalSize(350) },
-    { x: 900, y: proportionalSize(350) },
-    { x: 1050, y: proportionalSize(150) },
-
-    { x: 2500, y: proportionalSize(450) },
-    { x: 2900, y: proportionalSize(400) },
-    { x: 3150, y: proportionalSize(350) },
-
-    { x: 3900, y: proportionalSize(450) },
-    { x: 4200, y: proportionalSize(400) },
-    { x: 4400, y: proportionalSize(200) },
-    { x: 4700, y: proportionalSize(150) },
-];
-
-const platforms = platformPositions.map(
-    (platform) => new Platform(platform.x, platform.y)
-);
 
 const checkpointPositions = [
     // { x: 1170, y: proportionalSize(80), z: 1 },
