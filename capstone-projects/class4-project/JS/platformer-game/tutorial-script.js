dc32fb85902ab444b98292e9a1d65dd8fd5f9dbd
arabog <arababs2015@gmail.com>
2025-03-15 20:02:17 +0100

    Platformer Game: tutorial-script.js: 297

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -161,8 +161,10 @@ class Player {}
 
 Lesson 13:
 Inside your Player class, you will need to define the 
-player's position, velocity, width, and height values. All 
-of these values will be defined inside the constructor method.
+player's position, velocity, width, and height values. 
+
+All of these values will be defined inside the constructor 
+method.
 
 Create an empty constructor inside your Player class.
 
