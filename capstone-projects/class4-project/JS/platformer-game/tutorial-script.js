067d1903011310fe3a3ecbd486963d02381497ce
arabog <arababs2015@gmail.com>
2025-03-15 20:13:40 +0100

    Platformer Game: tutorial-script.js: 303

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -283,9 +283,9 @@ draw() {
 
 
 Lesson 20:
-Below your ctx.fillStyle, you need to create the player's shape 
-by calling the fillRect() method on the ctx object which you 
-instantiated earlier.
+Below your ctx.fillStyle, you need to create the player's 
+shape by calling the fillRect() method on the ctx object 
+which you instantiated earlier.
 
 fillRect(x, y, width, height)
 
@@ -294,6 +294,7 @@ this.position.y, this.width and this.height values.
 
 draw() {
     ctx.fillStyle = "#99c9ff";
+    
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
 }
 
