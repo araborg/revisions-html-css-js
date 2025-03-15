28a8c57167e376ed6951ea67b96d6b560460da7a
arabog <arababs2015@gmail.com>
2025-03-15 19:47:02 +0100

    Platformer Game: tutorial-script.js: 291

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -98,12 +98,12 @@ canvas.height = innerHeight;
 
 
 Lesson 8:
-In your platformer game, the main player will need to jump between 
-the different platforms. When the player jumps, you will need to 
-apply gravity to bring them back down.
+In your platformer game, the main player will need to jump 
+between the different platforms. When the player jumps, you 
+will need to apply gravity to bring them back down.
 
-Create a new const variable called gravity and assign it the number 
-0.5.
+Create a new const variable called gravity and assign it 
+the number 0.5.
 
 const gravity = 0.5;
 
