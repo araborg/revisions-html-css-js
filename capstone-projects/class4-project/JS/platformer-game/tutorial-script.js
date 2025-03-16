2c0b80e07b6a4b9396cda8bbbacde50cd23bc821
arabog <arababs2015@gmail.com>
2025-03-16 18:49:31 +0100

    Platformer Game: tutorial-script.js: 414

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1452,17 +1452,24 @@ platforms.forEach((platform) => {
 
 
 Lesson 85:
-Add one last boolean expression that checks if the player's x 
-position is less than or equal to the sum of the platform's x 
-position plus the platform's width minus one-third of the 
+Add one last boolean expression that checks if the player's 
+x position is less than or equal to the sum of the platform's 
+x position plus the platform's width minus one-third of the 
 player's width.
 
 platforms.forEach((platform) => {
     const collisionDetectionRules = [
-        player.position.y + player.height <= platform.position.y,
-        player.position.y + player.height + player.velocity.y >= platform.position.y,
-        player.position.x >= platform.position.x - player.width / 2,
-        player.position.x <= platform.position.x + platform.width - player.width / 3,
+        player.position.y + player.height <= 
+            platform.position.y,
+
+        player.position.y + player.height + player.velocity.y >= 
+            platform.position.y,
+
+        player.position.x >= 
+            platform.position.x - player.width / 2,
+
+        player.position.x <= 
+            platform.position.x + platform.width - player.width / 3,
     ];
 });
 
