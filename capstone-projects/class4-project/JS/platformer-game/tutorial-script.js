66f28401712ac94cd595ab0c557cca726de13f58
arabog <arababs2015@gmail.com>
2025-03-16 18:46:33 +0100

    Platformer Game: tutorial-script.js: 411

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1416,14 +1416,17 @@ platforms.forEach((platform) => {
 
 
 Lesson 83:
-Add another boolean expression that checks if the sum of the player's 
-y position, height, and y velocity is greater than or equal to the 
-platform's y position.
+Add another boolean expression that checks if the sum of 
+the player's y position, height, and y velocity is greater 
+than or equal to the platform's y position.
 
 platforms.forEach((platform) => {
     const collisionDetectionRules = [
-        player.position.y + player.height <= platform.position.y,
-        player.position.y + player.height + player.velocity.y >= platform.position.y,
+        player.position.y + player.height <= 
+            platform.position.y,
+
+        player.position.y + player.height + player.velocity.y >= 
+            platform.position.y,
     ];
 });
 
