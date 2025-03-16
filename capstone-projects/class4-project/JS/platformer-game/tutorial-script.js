05ba24c01682b89cd429c3e7d8b9778f342e78f0
arabog <arababs2015@gmail.com>
2025-03-16 18:50:11 +0100

    Platformer Game: tutorial-script.js: 415

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1476,14 +1476,15 @@ platforms.forEach((platform) => {
 
 Lesson 86:
 Next, add an if statement that checks if every rule in the 
-collisionDetectionRules array is true. Make sure to use the every 
-method for this.
+collisionDetectionRules array is true. Make sure to use the 
+every method for this.
 
-Inside the body of the if statement, assign the number 0 to the 
-player's y velocity followed by a return statement.
+Inside the body of the if statement, assign the number 0 to 
+the player's y velocity followed by a return statement.
 
 if (collisionDetectionRules.every(rule => rule)) {
     player.velocity.y = 0;
+    
     return;
 }
 
