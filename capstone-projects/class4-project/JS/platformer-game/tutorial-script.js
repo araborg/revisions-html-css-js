1e7cc2afc57e41e561d3e59b5027dad4509051d4
arabog <arababs2015@gmail.com>
2025-03-16 14:02:59 +0100

    Platformer Game: tutorial-script.js: 387

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1039,9 +1039,9 @@ window.addEventListener("keydown", ({ key }) => {
 
 
 Lesson 61:
-Add another addEventListener to the global window object and pass 
-in the keyup event and use destructuring to pass in the key property 
-from the event.
+Add another addEventListener to the global window object 
+and pass in the "keyup" event and use destructuring to 
+pass in the key property from the event.
 
 window.addEventListener("keyup", ({ key }) => {});
 
