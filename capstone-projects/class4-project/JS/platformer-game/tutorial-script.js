894a4eba45ab1ded5ce3d13cad6791362cfa720b
arabog <arababs2015@gmail.com>
2025-03-16 13:57:40 +0100

    Platformer Game: tutorial-script.js: 380

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -975,30 +975,39 @@ If so, assign the xVelocity to player.velocity.x.
 Below that if statement, use the addition assignment operator 
 to assign the xVelocity to player.velocity.x.
 
-  switch (key) {
-    case "ArrowLeft":
-        keys.leftKey.pressed = isPressed;
+const movePlayer = (key, xVelocity, isPressed) => {
+    if (!isCheckpointCollisionDetectionActive) {
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        
+        return;
+    }
 
-        if (xVelocity === 0) {
-            player.velocity.x = xVelocity;
-        }
-
-        player.velocity.x -= xVelocity;
-        break;
-
-    case "ArrowUp":
-    case " ":
-    case "Spacebar":
-        player.velocity.y -= 8;
-        break;
-
-    case  "ArrowRight":
-        keys.rightKey.pressed = isPressed;
-        if (xVelocity === 0) {
-            player.velocity.x = xVelocity;
-        }
-
-        player.velocity.x += xVelocity;
+    switch (key) {
+      case "ArrowLeft":
+          keys.leftKey.pressed = isPressed;
+  
+          if (xVelocity === 0) {
+              player.velocity.x = xVelocity;
+          }
+  
+          player.velocity.x -= xVelocity;
+          break;
+  
+      case "ArrowUp":
+      case " ":
+      case "Spacebar":
+          player.velocity.y -= 8;
+          break;
+  
+      case  "ArrowRight":
+          keys.rightKey.pressed = isPressed;
+          if (xVelocity === 0) {
+              player.velocity.x = xVelocity;
+          }
+  
+          player.velocity.x += xVelocity;
+  }
 }
 
 
