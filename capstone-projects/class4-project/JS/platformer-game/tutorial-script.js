4c6e2fb37efb6a001c076150aac1783c4f28a7fd
arabog <arababs2015@gmail.com>
2025-03-16 14:00:31 +0100

    Platformer Game: tutorial-script.js: 385

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1027,8 +1027,8 @@ window.addEventListener('keydown', ({ key }) => { })
 
 
 Lesson 60:
-Inside the arrow function, call the movePlayer function and pass in 
-key, 8, and true as arguments.
+Inside the arrow function, call the movePlayer function and 
+pass in key, 8, and true as arguments.
 
 window.addEventListener("keydown", ({ key }) => {
     movePlayer(key, 8, true);
