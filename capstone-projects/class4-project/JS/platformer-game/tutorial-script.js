72bceccf786a3ef13374f33cfae5e8c2affe02eb
arabog <arababs2015@gmail.com>
2025-03-16 06:01:33 +0100

    Platformer Game: tutorial-script.js: 352

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -695,16 +695,23 @@ const keys = {}
 
 
 Lesson 45:
-Inside the keys object, add a new key called rightKey and assign 
-it an object with the key-value pair of pressed: false.
+Inside the keys object, add a new key called rightKey 
+and assign it an object with the key-value pair of 
+pressed: false.
 
-Below the rightKey object, create a leftKey object and assign it 
-an object with the key-value pair of pressed: false.
+Below the rightKey object, create a leftKey object and 
+assign it an object with the key-value pair of 
+pressed: false.
 
 const keys = {
-    rightKey: {pressed: false},
-    leftKey: {pressed: false},
-}
+    rightKey: {
+        pressed: false,
+    },
+
+    leftKey: {
+        pressed: false,
+    },
+};
 
 
 Lesson 46:
