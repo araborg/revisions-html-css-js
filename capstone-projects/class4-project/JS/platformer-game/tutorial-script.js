890f9bb13d751e27c898f4650f04ccf6486306f4
arabog <arababs2015@gmail.com>
2025-03-16 19:20:37 +0100

    Platformer Game: tutorial-script.js: 430

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1737,20 +1737,20 @@ const checkpointPositions = [
 
 
 Lesson 101:
-The next step is to create a list of new checkpoint instances using 
-the CheckPoint class.
+The next step is to create a list of new checkpoint 
+instances using the CheckPoint class.
 
-Start by creating a new const variable called checkpoints and assign 
-it checkpointPositions.map().
+Start by creating a new const variable called checkpoints 
+and assign it checkpointPositions.map().
 
-For the map callback function, pass in checkpoint for the parameter 
-and implicitly return the creation of a new CheckPoint instance 
-with the checkpoint.x, checkpoint.y and checkpoint.z values passed 
-in as arguments.
+For the map callback function, pass in checkpoint for 
+the parameter and implicitly return the creation of a 
+new CheckPoint instance with the checkpoint.x, checkpoint.y 
+and checkpoint.z values passed in as arguments.
 
-const checkpoints = checkpointPositions
-                        .map(checkpoint 
-                        => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z))
+const checkpoints = checkpointPositions.map(
+    (checkpoint) => new CheckPoint(checkpoint.x, checkpoint.y, checkpoint.z)
+)
 
 
 Lesson 102:
