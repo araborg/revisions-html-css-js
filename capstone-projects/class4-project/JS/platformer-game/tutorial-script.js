0d18176f727de65ba53630ad73d93243f725d26d
arabog <arababs2015@gmail.com>
2025-03-16 05:47:26 +0100

    Platformer Game: tutorial-script.js: 343

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -606,13 +606,14 @@ const startGame = () => {
 }
 
 Lesson 39:
-Now it's time to add the functionality for the start game button.
+Now it's time to add the functionality for the start game 
+button.
 
-Add an addEventListener to the startBtn and pass in a click event 
-and a reference to the startGame function.
+Add an addEventListener to the startBtn and pass in a 
+click event and a reference to the startGame function.
 
-Click on the start game button, and you should see a light blue 
-square on the screen which represents the main player.
+Click on the start game button, and you should see a light 
+blue square on the screen which represents the main player.
 
 startBtn.addEventListener('click', startGame)
 
