722dc2f6acd8bb37e098c9bfa51fe66e61c752b2
arabog <arababs2015@gmail.com>
2025-03-16 18:44:24 +0100

    Platformer Game: tutorial-script.js: 410

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1401,15 +1401,17 @@ platforms.forEach(platform => {});
 
 
 Lesson 82:
-Inside the callback function, create a new const variable called 
-collisionDetectionRules and assign it an empty array.
+Inside the callback function, create a new const variable 
+called collisionDetectionRules and assign it an empty array.
 
-Inside that array, add a boolean expression that checks whether the 
-player's y position plus the player's height is less than or equal 
-to the platform's y position.
+Inside that array, add a boolean expression that checks 
+whether the player's y position plus the player's height 
+is less than or equal to the platform's y position.
 
 platforms.forEach((platform) => {
-    const collisionDetectionRules = [player.position.y + player.height <= platform.position.y];
+    const collisionDetectionRules = [
+        player.position.y + player.height <= platform.position.y,
+    ];
 });
 
 
