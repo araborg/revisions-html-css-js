fef51145f8cd345022066045c2c523544333edc6
arabog <arababs2015@gmail.com>
2025-03-16 05:35:46 +0100

    Platformer Game: tutorial-script.js: 336

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -525,8 +525,8 @@ if(this.position.x < this.width) {}
 
 
 Lesson 32:
-Inside the if statement, assign the width to the player's 
-x position.
+Inside the if statement, assign the width to the player's x 
+position.
 
 if (this.position.x < this.width) {
     this.position.x = this.width;
