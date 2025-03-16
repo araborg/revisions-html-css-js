318ef723c3c5c6a28a18a22d06cd2d0d2ec6ce08
arabog <arababs2015@gmail.com>
2025-03-16 19:34:13 +0100

    Platformer Game: tutorial-script.js: 434

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1956,11 +1956,13 @@ const animateIt = () => {
 
 
 Lesson 105:
-The next step is to create a function that will show the checkpoint 
-message when the player reaches a checkpoint.
+The next step is to create a function that will show the 
+checkpoint message when the player reaches a checkpoint.
 
-Create a new arrow function called showCheckpointScreen that takes 
-in a msg parameter.
+Create a new arrow function called showCheckpointScreen 
+that takes in a msg parameter.
+
+const showCheckpointScreen = (msg) => {}
 
 
 Lesson 106:
