997543860da00bba6892714b449dd26a64113544
arabog <arababs2015@gmail.com>
2025-03-16 18:04:46 +0100

    Platformer Game: tutorial-script.js: 402

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1315,10 +1315,13 @@ of the platforms onto the canvas.
 Add a forEach loop that iterates through the platforms 
 array.
 
-Inside the callback function, add a platform parameter and for 
-the body of the function call the draw method on each platform.
+Inside the callback function, add a platform parameter 
+and for the body of the function call the draw method 
+on each platform.
 
-platforms.forEach(platform => platform.draw());
+platforms.forEach((platform) => {
+    platform.draw()
+});
 
 
 Lesson 78:
