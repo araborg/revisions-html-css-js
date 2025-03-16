eabf0a174ddaad6cad03c87f87b84cab1d791844
arabog <arababs2015@gmail.com>
2025-03-16 18:52:34 +0100

    Platformer Game: tutorial-script.js: 418

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1502,7 +1502,8 @@ the player's x position is greater than or equal to the
 platform's x position minus half of the player's width.
 
 const platformDetectionRules = [
-    player.position.x >= platform.position.x - player.width / 2,
+    player.position.x >= 
+        platform.position.x - player.width / 2,
 ];
 
 
