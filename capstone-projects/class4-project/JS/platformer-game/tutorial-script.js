6d742eb2ea61e3ea31c95c8e0459b669588cdbf4
arabog <arababs2015@gmail.com>
2025-03-16 05:48:14 +0100

    Platformer Game: tutorial-script.js: 344

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -619,8 +619,9 @@ startBtn.addEventListener('click', startGame)
 
 
 Lesson 40:
-Now that you can see the player on the screen, it is time to start 
-adding the functionality for moving the player across the screen.
+Now that you can see the player on the screen, it is 
+time to start adding the functionality for moving the 
+player across the screen.
 
 Create a new empty arrow function called animate.
 
