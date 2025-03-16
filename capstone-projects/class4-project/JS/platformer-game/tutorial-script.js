e0db1252a4b4c1a9801987cda719f95ce6c2db48
arabog <arababs2015@gmail.com>
2025-03-16 05:11:44 +0100

    Platformer Game: tutorial-script.js: 321

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -457,13 +457,26 @@ Lesson 29:
 Below your inner if statement, use the addition assignment 
 operator to add gravity to the y velocity.
 
-if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    if (this.position.y < 0) {
-        this.position.y = 0;
-        this.velocity.y = gravity;
-    }
+update() {
+    this.draw();
 
-    this.velocity.y += gravity;
+    this.position.x += this.velocity.x; 
+    
+    this.position.y += this.velocity.y;
+
+    if (
+            this.position.y  + 
+            this.height + 
+            this.velocity.y <= canvas.height
+        ) {
+            if (this.position.y < 0) {
+                this.position.y = 0
+
+                this.velocity.y = gravity;
+            }
+        }
+
+        this.velocity.y += gravity;
 }
 
 
