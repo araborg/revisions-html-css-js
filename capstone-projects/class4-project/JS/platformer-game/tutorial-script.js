861472a31389dc53efc3cf6af321ecb025c63be4
arabog <arababs2015@gmail.com>
2025-03-16 19:16:43 +0100

    Platformer Game: tutorial-script.js: 428

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1667,7 +1667,7 @@ class CheckPoint {
         
         this.claimed = false;
     }
-        
+
     draw(){
         ctx.fillStyle = "#f1be32";
     
@@ -1677,22 +1677,45 @@ class CheckPoint {
 
 
 Lesson 99:
-The last method you will need to add to the CheckPoint class is the 
-claim method.
+The last method you will need to add to the CheckPoint class 
+is the claim method.
 
-Inside the claim method, assign 0 to the width and height properties 
-of the CheckPoint instance.
+Inside the claim method, assign 0 to the width and height 
+properties of the CheckPoint instance.
 
 Below those properties, assign Infinity to the y position.
 
 Lastly, assign true to the claimed property.
 
-claim() {
-    this.width = 0;
-    this.height = 0;
-    this.position.y = Infinity;
-    this.claimed = true;
+class CheckPoint {
+    constructor(x, y, z) {
+        this.position = {
+            x,
+            y,
+        };
+
+        this.width = proportionalSize(40); // 40
+        this.height = proportionalSize(70); // 70
+        
+        this.claimed = false;
+    }
+
+    draw(){
+        ctx.fillStyle = "#f1be32";
+    
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    claim() {
+        this.width = 0;
+        this.height = 0;
+
+        this.position.y = Infinity;
+        
+        this.claimed = true;
+    }
 }
+
 
 
 Lesson 100:
