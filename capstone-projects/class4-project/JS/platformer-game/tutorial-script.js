38c719d660eb631851f0780884dc3ab2ffa47d20
arabog <arababs2015@gmail.com>
2025-03-16 05:17:03 +0100

    Platformer Game: tutorial-script.js: 325

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -483,15 +483,28 @@ update() {
 Lesson 30:
 Add an else clause that assigns 0 to this.velocity.y.
 
-if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    if (this.position.y < 0) {
-        this.position.y = 0;
-        this.velocity.y = gravity;
-    }
-    this.velocity.y += gravity;
+update() {
+    this.draw();
 
-}else {
-    this.velocity.y = 0;
+    this.position.x += this.velocity.x; 
+    
+    this.position.y += this.velocity.y;
+
+    if (
+        this.position.y  + 
+        this.height + 
+        this.velocity.y <= canvas.height
+    ) {
+        if (this.position.y < 0) {
+            this.position.y = 0
+
+            this.velocity.y = gravity;
+        }
+
+        this.velocity.y += gravity;
+    }else {
+        this.velocity.y = 0;
+    }
 }
 
 
