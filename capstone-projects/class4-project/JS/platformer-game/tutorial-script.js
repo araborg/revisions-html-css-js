9e8c54e294d68cff9beddc0e9ea202eca86cc7be
arabog <arababs2015@gmail.com>
2025-03-16 05:23:21 +0100

    Platformer Game: tutorial-script.js: 329

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -513,8 +513,6 @@ The final condition you need to add inside the Player class is
 to ensure that the player stays within the boundaries of the 
 canvas screen and doesn't move too far off to the left.
 
-
-
 Create an if statement, to check if the player's x position 
 is less than the width (of the player).
 
