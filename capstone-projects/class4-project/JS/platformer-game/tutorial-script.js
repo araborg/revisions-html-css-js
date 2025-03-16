1dfd9d2e7fa3fcfdd367d1de13219d9a9cae9f5b
arabog <arababs2015@gmail.com>
2025-03-16 08:04:28 +0100

    Platformer Game: tutorial-script.js: 361

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -778,9 +778,15 @@ Lesson 49:
 Add an else clause that assigns the number 0 to the player's 
 x velocity.
 
-if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+if (
+    keys.rightKey.pressed && 
+    player.position.x < proportionalSize(400)
+) {
     player.velocity.x = 5;
-} else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
+} else if(
+        keys.leftKey.pressed &&
+        player.position.x > proportionalSize(100) // 70
+) {
     player.velocity.x = -5;
 } else {
     player.velocity.x = 0;
