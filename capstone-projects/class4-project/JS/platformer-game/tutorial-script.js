2493275f6457d48d9eaa8ea8d23d61c799bd74c3
arabog <arababs2015@gmail.com>
2025-03-16 18:05:37 +0100

    Platformer Game: tutorial-script.js: 403

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1325,16 +1325,16 @@ platforms.forEach((platform) => {
 
 
 Lesson 78:
-If you try to start the game, you will notice that the platforms are 
-rendered on the screen. But as the player moves to the right, the 
-platform does not move with it.
+If you try to start the game, you will notice that the 
+platforms are rendered on the screen. But as the player 
+moves to the right, the platform does not move with it.
 
-To fix this issue, you will need to update the platform's x position 
-as the player moves across the screen.
+To fix this issue, you will need to update the platform's 
+x position as the player moves across the screen.
 
-Inside the animate function, add a condition to check if the right 
-key was pressed and if the isCheckpointCollisionDetectionActive 
-is true.
+Inside the animate function, add a condition to check 
+if the right key was pressed and if the 
+isCheckpointCollisionDetectionActive is true.
 
 if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {}
 
