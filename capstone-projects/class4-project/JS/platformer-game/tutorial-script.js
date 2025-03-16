44386ea3fd36cb17b31ab45b5a4b78f6a4d52997
arabog <arababs2015@gmail.com>
2025-03-16 08:02:03 +0100

    Platformer Game: tutorial-script.js: 359

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -752,9 +752,11 @@ if (
 Lesson 48:
 Add an else if statement where the condition checks if the 
 left key was pressed and the player's x position is greater 
-than proportionalSize(100). You need to use the proportionalSize 
-function here to make sure the player's x position is always 
-proportional to the screen size.
+than proportionalSize(100). Y
+
+ou need to use the proportionalSize function here to make 
+sure the player's x position is always proportional to 
+the screen size.
 
 Inside the else if statement, assign the number -5 to the 
 player's x velocity.
