33c0cc56f35161f21e8a0d28c8cacdd1027555fc
arabog <arababs2015@gmail.com>
2025-03-16 18:39:32 +0100

    Platformer Game: tutorial-script.js: 409

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -1386,16 +1386,16 @@ if (
 
 
 Lesson 81:
-When you start the game, you will notice that the position of the 
-platforms is animating alongside the player. But if you try to 
-jump below one of the platforms, then you will jump right through 
-it.
+When you start the game, you will notice that the position 
+of the platforms is animating alongside the player. But if 
+you try to jump below one of the platforms, then you will 
+jump right through it.
 
-To fix this issue, you will need to add collision detection logic 
-to the game.
+To fix this issue, you will need to add collision detection 
+logic to the game.
 
-Start by calling the forEach method on the platforms array. For 
-the callback function pass in platform as the parameter.
+Start by calling the forEach method on the platforms array. 
+For the callback function pass in platform as the parameter.
 
 platforms.forEach(platform => {});
 
