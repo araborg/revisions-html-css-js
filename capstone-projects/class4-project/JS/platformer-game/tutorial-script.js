e7faf2a7773257c76657a780809be9c3f7af3a71
arabog <arababs2015@gmail.com>
2025-03-16 05:08:19 +0100

    Platformer Game: tutorial-script.js: 317

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -388,8 +388,20 @@ Lesson 26:
 In the if statement, add another if statement to check if the 
 player's y position is less than 0.
 
-if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-    if (this.position.y < 0) {}
+update() {
+    this.draw();
+
+    this.position.x += this.velocity.x; 
+    
+    this.position.y += this.velocity.y;
+
+    if (
+            this.position.y  + 
+            this.height + 
+            this.velocity.y <= canvas.height
+        ) {
+            if (this.position.y < 0) {}
+        }
 }
 
 
