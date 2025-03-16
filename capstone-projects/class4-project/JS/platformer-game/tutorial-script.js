f0b7deeb9bb6781e6605059033db50565502e252
arabog <arababs2015@gmail.com>
2025-03-16 19:46:48 +0100

    Platformer Game: tutorial-script.js: 442

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -2168,6 +2168,7 @@ close enough to the checkpoint to claim it.
 
 The second rule should check if index is strictly equal to 0 
 or if the previous checkpoints[index - 1].claimed is true. 
+
 This will ensure that the player can only claim the first 
 checkpoint or a checkpoint that has already been claimed.
 
@@ -2195,7 +2196,25 @@ Next, add an if statement that checks if every rule in the
 checkpointDetectionRules array is true.
 Make sure to use the every method for this.
 
-if (checkpointDetectionRules.every(rule => rule)) {}
+checkpoints.forEach((checkpoint, index, checkpoints) => {
+    const checkpointDetectionRules =[
+        player.position.x >= checkpoint.position.x,
+
+        player.position.y >= checkpoint.position.y,
+
+        player.position.y + player.height <= 
+            checkpoint.position.y + checkpoint.height,
+
+        isCheckpointCollisionDetectionActive
+
+        player.position.x - player.width <= 
+            checkpoint.position.x - checkpoint.width + player.width * 0.9,
+        
+        index === 0 || checkpoints[index - 1].claimed === true,
+    ]
+        
+    if (checkpointDetectionRules.every(rule => rule)) {}
+});
 
 
 Lesson 114:
