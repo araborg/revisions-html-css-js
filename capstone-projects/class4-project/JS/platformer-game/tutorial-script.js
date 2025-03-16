7f394ee8da292cb5f2ecd52f560ae2db70cc4ca2
arabog <arababs2015@gmail.com>
2025-03-16 05:46:28 +0100

    Platformer Game: tutorial-script.js: 341

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -577,13 +577,15 @@ Lesson 37:
 Inside your startGame function, you will need to display the 
 canvas element and hide the startScreen container.
 
-Use canvas.style.display to change the display value to "block".
+Use canvas.style.display to change the display value to 
+"block".
 
 Below that, use startScreen.style.display to change the 
 display value to "none".
 
 const startGame = () => {
     canvas.style.display = "block";
+    
     startScreen.style.display = "none";
 }
 
