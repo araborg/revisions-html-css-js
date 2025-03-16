b5430bb6d56be47de56d2d514e72b0bb47dd31d5
arabog <arababs2015@gmail.com>
2025-03-16 05:48:47 +0100

    Platformer Game: tutorial-script.js: 345

--- capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
+++ capstone-projects/class4-project/JS/platformer-game/tutorial-script.js
@@ -629,13 +629,14 @@ const animate = () => {}
 
 
 Lesson 41:
-The requestAnimationFrame() web API, takes in a callback and is used 
-to update the animation on the screen. The animate function will be 
-responsible for updating the player's position and continually 
-drawing it on the canvas.
+The requestAnimationFrame() web API, takes in a callback 
+and is used to update the animation on the screen. The 
+animate function will be responsible for updating the 
+player's position and continually drawing it on the 
+canvas.
 
-Inside the animate function, call the requestAnimationFrame() API 
-and pass animate as the argument.
+Inside the animate function, call the requestAnimationFrame() 
+API and pass animate as the argument.
 
 const animate = () => {
     requestAnimationFrame(animate);
