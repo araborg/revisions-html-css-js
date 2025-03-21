67093741990d75bf7886915daf0347ef721f849b
arabog <arababs2015@gmail.com>
2025-03-21 17:13:20 +0100

    Dice game-Updated revision-1: 7

--- revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
@@ -6,6 +6,6 @@ const scoreSpans = document.querySelectorAll("#score-options span");
 const roundElement = document.getElementById("current-round");
 const rollsElement = document.getElementById("current-round-rolls");
 const totalScoreElement = document.getElementById("total-score");
-const scoreHistoryElement = document.getElementById("score-history");
+const scoreHistory = document.getElementById("score-history");
 
 console.log(scoreInputs, scoreSpans);
