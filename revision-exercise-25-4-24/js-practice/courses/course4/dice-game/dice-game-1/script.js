3912518d8449e8b37923a2d4866aa2e3e8199939
arabog <arababs2015@gmail.com>
2025-03-21 17:12:26 +0100

    Dice game-Updated revision-1: 6

--- revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
@@ -1,5 +1,11 @@
 const listOfAllDice = document.querySelectorAll(".die");
+
 const scoreInputs = document.querySelectorAll("#score-options input");
 const scoreSpans = document.querySelectorAll("#score-options span");
+
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistoryElement = document.getElementById("score-history");
 
 console.log(scoreInputs, scoreSpans);
