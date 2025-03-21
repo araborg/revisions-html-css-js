e27c70ff5e9b0ee9501be1102c8604bdf42fb0a4
arabog <arababs2015@gmail.com>
2025-03-21 17:38:07 +0100

    Dice game-Updated revision-1: 16

--- revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
@@ -10,12 +10,15 @@ const scoreHistory = document.getElementById("score-history");
 
 const rulesBtn = document.getElementById("rules-btn");
 const rulesContainer = document.querySelector(".rules-container");
+
 const keepScoreBtn = document.querySelector("#keep-score-btn");
 const rollDiceBtn = document.querySelector("#roll-dice-btn");
 
-let isModalShowing;
+let isModalShowing = [];
 let diceValuesArr;
 
 let rolls = 0;
 let currentScore = 0;
 let round = 1;
+
+rulesBtn.addEventListener("click", () => {});
