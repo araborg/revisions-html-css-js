fd437701d2809f921456446d46559b904b4f081a
arabog <arababs2015@gmail.com>
2025-03-21 21:52:36 +0100

    Dice game-Updated tutorial-script 01: 14

--- revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
@@ -51,5 +51,11 @@ const rollDice = () => {
 };
 
 rollDiceBtn.addEventListener("click", () => {
-    rollDice();
+    if (rolls === 3) {
+        alert("You have made three rolls this round. Please select a score.");
+    } else {
+        rolls++;
+
+        rollDice();
+    }
 });
