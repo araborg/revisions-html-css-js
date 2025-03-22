0ce181092f8f91cf1b17a8a33bdc8584e7740882
arabog <arababs2015@gmail.com>
2025-03-22 19:59:41 +0100

    Dice game-Updated 01: 74

--- revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course4/dice-game/dice-game-1/script.js
@@ -200,6 +200,8 @@ rollDiceBtn.addEventListener("click", () => {
 
         getHighestDuplicates(diceValuesArr);
         detectFullHouse(diceValuesArr);
+
+        checkForStraights(diceValuesArr);
     }
 });
 
