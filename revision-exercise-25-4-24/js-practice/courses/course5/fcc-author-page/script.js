f1d31d631abe94d8359f8f4a36bb8ac0a341858c
arabog <arababs2015@gmail.com>
2025-03-26 03:36:01 +0100

    fCC Authors Page: 14

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-author-page/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-author-page/script.js
@@ -1,18 +1,22 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
-fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
-    .then((res) => res.json())
-    .then((data) => (authorDataArr = data))
-    .catch((err) => console.error(`There was an error: ${err}`));
-
 let startingIndex = 0;
 let endingIndex = 8;
 
 let authorDataArr = [];
 
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+    .then((res) => res.json())
+    .then((data) => {
+        authorDataArr = data;
+
+        displayAuthors(authorDataArr);
+    })
+    .catch((err) => console.error(`There was an error: ${err}`));
+
 const displayAuthors = (authors) => {
-    authors.forEach({ author, image, url, bio }, (index) => {
+    authors.forEach(({ author, image, url, bio }, index) => {
         authorContainer += `
             <div id=${index} class="user-card">
                 <h2 class="author-name">${author}<h2>
