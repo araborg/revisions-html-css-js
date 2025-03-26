18d39160ca8947856f3be0b78ec222d89f77c3ff
arabog <arababs2015@gmail.com>
2025-03-26 03:10:17 +0100

    fCC Authors Page: 03

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-author-page/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-author-page/script.js
@@ -3,4 +3,5 @@ const loadMoreBtn = document.getElementById("load-more-btn");
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data))
+    .catch((err) => console.error(errr));
