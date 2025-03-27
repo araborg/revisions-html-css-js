719ec2acac58a4991dddcf14cabbbd2b34fa0483
arabog <arababs2015@gmail.com>
2025-03-27 17:37:49 +0100

    fCC forum leaderboard: 46

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
@@ -105,4 +105,14 @@ const allCategories = {
         category: "Career Advice",
         className: "career",
     },
+
+    409: {
+        category: "Project Feedback",
+        className: "feedback",
+    },
+
+    417: {
+        category: "freeCodeCamp Support",
+        className: "support",
+    },
 };
