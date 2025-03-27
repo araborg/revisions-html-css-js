ce10df4ecc17fd045802563d8a2e0f28e2de8592
arabog <arababs2015@gmail.com>
2025-03-27 03:38:07 +0100

    fCC forum leaderboard: 6

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
@@ -2,3 +2,5 @@ const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
