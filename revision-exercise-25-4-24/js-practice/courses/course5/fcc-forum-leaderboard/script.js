8a3d3f6670cef45207f954aa9e2dbf292e101c29
arabog <arababs2015@gmail.com>
2025-03-27 17:02:16 +0100

    fCC forum leaderboard: 36

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
@@ -71,6 +71,7 @@ const timeAgo = (time) => {
 
     const timeDifference = currentTime - lastPost;
 
+    // one minute = 1000 * 60 milliseconds
     const msPerMinute = 1000 * 60;
 
     const minutesAgo = Math.floor(timeDifference / msPerMinute);
