edba2228e2deb590de6d9f227edafa8b69b78d8f
arabog <arababs2015@gmail.com>
2025-03-27 19:51:10 +0100

    fCC forum leaderboard: 76

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
@@ -6,14 +6,45 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
 
 const allCategories = {
-    299: { category: "Career Advice", className: "career" },
-    409: { category: "Project Feedback", className: "feedback" },
-    417: { category: "freeCodeCamp Support", className: "support" },
-    421: { category: "JavaScript", className: "javascript" },
-    423: { category: "HTML - CSS", className: "html-css" },
-    424: { category: "Python", className: "python" },
-    432: { category: "You Can Do This!", className: "motivation" },
-    560: { category: "Backend Development", className: "backend" },
+    299: {
+        category: "Career Advice",
+        className: "career",
+    },
+
+    409: {
+        category: "Project Feedback",
+        className: "feedback",
+    },
+
+    417: {
+        category: "freeCodeCamp Support",
+        className: "support",
+    },
+
+    421: {
+        category: "JavaScript",
+        className: "javascript",
+    },
+
+    423: {
+        category: "HTML - CSS",
+        className: "html-css",
+    },
+
+    424: {
+        category: "Python",
+        className: "python",
+    },
+
+    432: {
+        category: "You Can Do This!",
+        className: "motivation",
+    },
+
+    560: {
+        category: "Backend Development",
+        className: "backend",
+    },
 };
 
 const forumCategory = (id) => {
