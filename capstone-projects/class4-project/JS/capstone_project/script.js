99f1c6227258c76f2bd89644455f651c29a8b043
arabog <arababs2015@gmail.com>
2025-03-28 19:31:24 +0100

    Capstone project 4: 44

---
+++ capstone-projects/class4-project/JS/capstone_project/script.js
@@ -0,0 +1,13 @@
+let price = 3.26;
+
+let cid = [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+];
