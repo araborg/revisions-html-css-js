e6c09ed8b807a8775a235de936f6f66469dc30e4
arabog <arababs2015@gmail.com>
2025-03-28 15:57:08 +0100

    Capstone project 4: 12

--- capstone-projects/class4-project/JS/capstone_project/script.js
+++ capstone-projects/class4-project/JS/capstone_project/script.js
@@ -28,7 +28,7 @@ and bills sorted in highest to lowest order.
 */
 
 // the price of the item
-let price = 1.87;
+// let price = 1.87;
 
 // cash-in-drawer
 let cid = [
@@ -44,17 +44,71 @@ let cid = [
 ];
 
 const cashEl = document.getElementById("cash");
-
 const purchaseBtn = document.getElementById("purchase-btn");
 
+const changeDue = document.getElementById("change-due");
+
+// let price = 1.87;
+// let price = 19.5;
+let price = 3.26;
+
+let currency = [
+    ["PENNY", 0.01],
+    ["NICKEL", 0.05],
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 20],
+];
+
+let cashValue;
+let diff;
+
 const getPrice = () => {
-    const value = cashEl.value;
+    cashValue = parseFloat(cashEl.value);
 
-    // return (cash = parseFloat(value));
+    if (cashValue < price) {
+        alert("Customer does not have enough money to purchase the item");
+    }
 
-    console.log(value);
+    if (cashValue === price) {
+        changeDue.textContent = "No change due - customer paid with exact cash";
+    }
+
+    if (cashValue > price) {
+        cid.forEach((item, index) => {
+            const cashInDrawer = item[1];
+
+            diff = cashValue - price;
+
+            // console.log(cashInDrawer)
+            if (diff <= cashInDrawer) {
+                // console.log(cashInDrawer)
+                changeDue.textContent += `Status: OPEN ${item[0]}: $${diff}`;
+            }
+
+            if (diff >= cashInDrawer) {
+                console.log(cashInDrawer);
+            }
+
+            // else if (cashInDrawer >= cashValue) {
+            //   console.log(cashInDrawer);
+            //   console.log(cashValue);
+
+            // }
+        });
+
+        currency.forEach((curr) => {
+            // console.log(diff)
+            // console.log(typeof curr[1])
+
+            const reminder = diff % curr[1];
+            // console.log(reminder)
+        });
+    }
 };
-
-// console.log(getPrice());
 
 purchaseBtn.addEventListener("click", getPrice);
