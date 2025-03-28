24573c227d130a15245444bfdecd0eeedc0f0c57
arabog <arababs2015@gmail.com>
2025-03-28 22:03:24 +0100

    Capstone project 04 following e.g.: 21

--- capstone-projects/class4-project/JS/capstone_project/script.js
+++ capstone-projects/class4-project/JS/capstone_project/script.js
@@ -98,6 +98,7 @@ const checkCashRegister = () => {
 
     for (let i = 0; i <= reversedCid.length; i++) {
         if (changeDue >= denominations[i] && changeDue > 0) {
+            const [denominationName, total] = reversedCid[i];
         }
     }
 };
