42814249ae7d5ceb556e12c353c1610974e03ad2
arabog <arababs2015@gmail.com>
2025-03-28 11:11:55 +0100

    Capstone project 4: 6

--- capstone-projects/class4-project/JS/capstone_project/script.js
+++ capstone-projects/class4-project/JS/capstone_project/script.js
@@ -11,6 +11,18 @@ customer provides too little cash or
 when the cash drawer doesn't have enough to issue 
 the correct change.
 
+Msg:
+"Status: INSUFFICIENT_FUNDS": 
+if cash-in-drawer is less than the change due, or 
+if you cannot return the exact change.
+
+"Status: CLOSED": 
+if cash-in-drawer is equal to the change due.
+
+"Status: OPEN": 
+if cash-in-drawer is greater than the change due and 
+you can return change, with the change due in coins 
+and bills sorted in highest to lowest order.
 
 
 */
