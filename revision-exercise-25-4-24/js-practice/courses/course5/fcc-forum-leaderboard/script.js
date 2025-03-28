e6cd5578c907f9644ac5519a8d13902fe4bafcfe
arabog <arababs2015@gmail.com>
2025-03-28 01:12:34 +0100

    fCC forum leaderboard: 80

--- revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course5/fcc-forum-leaderboard/script.js
@@ -19,6 +19,47 @@ const fetchData = async () => {
 
 fetchData();
 
+const showLatestPosts = (data) => {
+    const { topic_list, users } = data;
+    const { topics } = topic_list;
+
+    postsContainer.innerHTML = topics
+        .map((item) => {
+            const {
+                id,
+                title,
+                views,
+                posts_count,
+                slug,
+                posters,
+                category_id,
+                bumped_at,
+            } = item;
+
+            return `
+                <tr>
+                    <td>
+                        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
+
+                        ${forumCategory(category_id)}
+                    </td>
+
+                    <td>
+                        <div class="avatar-container">
+                            ${avatars(posters, users)}
+                        </div>
+                    </td>
+                    
+                    <td>${posts_count - 1}</td>
+                    
+                    <td>${viewCount(views)}</td>
+                    
+                    <td>${timeAgo(bumped_at)}</td>
+                </tr>`;
+        })
+        .join("");
+};
+
 const allCategories = {
     299: {
         category: "Career Advice",
@@ -145,47 +186,6 @@ const avatars = (posters, users) => {
                     />
                 `;
             }
-        })
-        .join("");
-};
-
-const showLatestPosts = (data) => {
-    const { topic_list, users } = data;
-    const { topics } = topic_list;
-
-    postsContainer.innerHTML = topics
-        .map((item) => {
-            const {
-                id,
-                title,
-                views,
-                posts_count,
-                slug,
-                posters,
-                category_id,
-                bumped_at,
-            } = item;
-
-            return `
-                <tr>
-                    <td>
-                        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
-
-                        ${forumCategory(category_id)}
-                    </td>
-
-                    <td>
-                        <div class="avatar-container">
-                            ${avatars(posters, users)}
-                        </div>
-                    </td>
-                    
-                    <td>${posts_count - 1}</td>
-                    
-                    <td>${viewCount(views)}</td>
-                    
-                    <td>${timeAgo(bumped_at)}</td>
-                </tr>`;
         })
         .join("");
 };
