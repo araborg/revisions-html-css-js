6b07c019fd2c68aed954e1b562c04cd155eb8714
arabog <arababs2015@gmail.com>
2025-03-30 18:31:08 +0100

    pokemon-search-app: 14

--- capstone-projects/class5-project/JS/pokemon-app/script.js
+++ capstone-projects/class5-project/JS/pokemon-app/script.js
@@ -1,39 +1,26 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 
-// pokemons();
-
 let inputPoke;
 
-const findPokemon = (pokem) => {
-    // const inputValue = searchInput.value;
+const findPokemon = () => {
+    const inputValue = searchInput.value;
 
-    // const result = pokemons();
-
-    console.log(pokem);
+    pokemons(inputValue);
 };
 
 searchBtn.addEventListener("click", findPokemon);
 
 // console.log(inputPoke);
 
-// const pokemonUrl = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputPoke}`;
-// console.log(pokemonUrl);
-
-const pokemons = async () => {
+const pokemons = async (input) => {
     try {
-        const inputValue = searchInput.value;
-
         const res = await fetch(
-            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputValue}`
+            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${input}`
         );
 
         const pokem = await res.json();
-        findPokemon(pokem);
-
         console.log(pokem);
-
-        // findPokemon(pokem);
     } catch (err) {
         console.error(err);
     }
