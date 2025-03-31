34cb14c4d16bd4341e32e991a16bae602f122416
arabog <arababs2015@gmail.com>
2025-03-31 11:13:42 +0100

    cash register tutorial_script.js: 4

--- capstone-projects/class4-project/JS/capstone_project/script.js
+++ capstone-projects/class4-project/JS/capstone_project/script.js
@@ -16,3 +16,22 @@ let cid = [
 const cashEl = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
 const changeDue = document.getElementById("change-due");
+
+// logic
+const checkCashRegister = () => {
+    const cashInCents = Math.round(Number(cash.value) * 100);
+    const priceInCents = Math.round(price * 100);
+
+    if (cashInCents < priceInCents) {
+        alert("Customer does not have enough money to purchase the item");
+        cash.value = "";
+        return;
+    }
+
+    if (cashInCents === priceInCents) {
+        displayChangeDue.innerHTML =
+            "<p>No change due - customer paid with exact cash</p>";
+        cash.value = "";
+        return;
+    }
+};
