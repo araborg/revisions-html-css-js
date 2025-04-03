c430da1bb367913b948d47052fd327883753ce06
arabog <arababs2015@gmail.com>
2025-04-03 20:26:05 +0100

    Pyrmaid generator revision: 23

--- revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
@@ -61,7 +61,9 @@ const rows = [];
 const count = 8;
 const character = "#";
 
-for (let i = 0; i < count; i = i + 1) {}
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i);
+}
 
 // let pushed = rows.push("freeCodeCamp");
 // console.log(`This is d pushed value: ${pushed}`);
