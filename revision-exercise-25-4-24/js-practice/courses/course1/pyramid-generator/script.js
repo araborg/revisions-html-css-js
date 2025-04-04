7d26212d99e901b6971891524cc3bdbf87270fd3
arabog <arababs2015@gmail.com>
2025-04-04 23:47:31 +0100

    Pyrmaid generator revision: 154

--- revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
@@ -705,6 +705,8 @@ let inverted = true;
 for (let i = 1; i <= count; i++) {
     if (inverted) {
         rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
     }
 }
 
