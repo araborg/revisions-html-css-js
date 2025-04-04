f7c7f539a1660a948d86239a40c347853e954c89
arabog <arababs2015@gmail.com>
2025-04-04 20:55:11 +0100

    Pyrmaid generator revision: 102

--- revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/pyramid-generator/script.js
@@ -348,6 +348,7 @@ while (condition === true) {
     run logic
 }
 
+
 4.
 
 
@@ -397,19 +398,21 @@ for (let i = 1; i <= count; i++) {
 let continueLoop = false;
 let done = 0;
 
-for (const row of rows) {
-    result = result + "\n" + row;
-}
+while (continueLoop) {}
 
-function padRow(rowNumber, rowCount) {
-    return (
-        " ".repeat(rowCount - rowNumber) +
-        character.repeat(2 * rowNumber - 1) +
-        " ".repeat(rowCount - rowNumber)
-    );
-}
+// for (const row of rows) {
+//     result = result + "\n" + row;
+// }
 
-console.log(result);
+// function padRow(rowNumber, rowCount) {
+//     return (
+//         " ".repeat(rowCount - rowNumber) +
+//         character.repeat(2 * rowNumber - 1) +
+//         " ".repeat(rowCount - rowNumber)
+//     );
+// }
+
+// console.log(result);
 
 // stops at 70
 
