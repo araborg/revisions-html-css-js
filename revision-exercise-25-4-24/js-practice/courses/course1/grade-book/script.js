ac9561b5b3b38da642da30c526086de32ae2e130
arabog <arababs2015@gmail.com>
2025-04-05 18:13:22 +0100

    Grade Book App revision: 3

--- revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
@@ -1,5 +1,13 @@
 const getAverage = (scores) => {
-    console.log(scores);
+    // console.log(scores);
+
+    const sum = 0;
+
+    for (let score of scores) {
+        sum += score;
+    }
+
+    return sum;
 };
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89, 37]));
