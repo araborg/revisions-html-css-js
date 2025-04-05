fca4a3b2163b5595addcccf110766caacb09ad1d
arabog <arababs2015@gmail.com>
2025-04-05 18:19:57 +0100

    Grade Book App revision: 5

--- revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
@@ -21,5 +21,11 @@ const getGrade = (score) => {
         return "A";
     } else if (score >= 80) {
         return "B";
+    } else if (score >= 70) {
+        return "C";
+    } else if (score >= 60) {
+        return "D";
+    } else {
+        return "F";
     }
 };
