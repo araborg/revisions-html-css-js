08ca8223554d729a88d2997d82188f867d1b2ccc
arabog <arababs2015@gmail.com>
2025-04-06 00:24:39 +0100

    Grade Book App revision: 31

--- revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
+++ revision-exercise-25-4-24/js-practice/courses/course1/grade-book/script.js
@@ -113,9 +113,6 @@ console.log(individualGrade(grades));
 
 // Revision:
 
-// console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
-// console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
-
 /*
 `With the score of: ${score}. Your grade is: ${grade}. You ${
                 passed ? "failed" : "passed"
@@ -145,25 +142,21 @@ const getAverage = (grades) => {
     return Math.round(sum / grades.length);
 };
 
-console.log(getAverage(grades));
+// console.log(getAverage(grades));
 
 const getGrade = (grade) => {
-    // if (grade === 100) {
-    //     return "A++";
-    // } else if (grade >= 90) {
-    //     return "A";
-    // } else if (grade >= 80) {
-    //     return "B";
-    // } else if (grade >= 70) {
-    //     return "C";
-    // } else if (grade >= 60) {
-    //     return "D";
-    // } else {
-    //     return "F";
-    // }
-
-    switch (grade) {
-        case grade === 100:
+    if (grade === 100) {
+        return "A++";
+    } else if (grade >= 90) {
+        return "A";
+    } else if (grade >= 80) {
+        return "B";
+    } else if (grade >= 70) {
+        return "C";
+    } else if (grade >= 60) {
+        return "D";
+    } else {
+        return "F";
     }
 };
 
@@ -177,6 +170,39 @@ const knowGrades = (grade) => {
     return placeholder;
 };
 
-console.log(knowGrades(grades));
+// console.log(knowGrades(grades));
 
-const hasPassingGrade = (grade) => {};
+const hasPassingGrade = (grade) => {
+    return getGrade(grade) !== "F";
+};
+
+const reportGrade = (grades, studentGrade) => {
+    const grade = getGrade(studentGrade);
+    const average = getAverage(grades);
+
+    if (hasPassingGrade(studentGrade)) {
+        return `Your grade: ${studentGrade}. Class average: ${average}. Your grade: ${grade}. You passed the course.`;
+    } else {
+        return `Your grade: ${studentGrade}. Class average: ${average}. Your grade: ${grade}. You failed the course.`;
+    }
+};
+
+console.log(reportGrade([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+console.log(reportGrade([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
+
+const individualGrade = (grades) => {
+    let result = "";
+
+    for (let grade of grades) {
+        const studentGrade = getGrade(grade);
+
+        result +=
+            `With the score of: ${grade}. Your grade is: ${studentGrade}. You ${
+                hasPassingGrade(grade) ? "passed" : "failed"
+            } the course.` + "\n";
+    }
+
+    return result;
+};
+
+console.log(individualGrade(grades));
