0318d621568e9db55b6ad56717c7b1964d1de6da
arabog <arababs2015@gmail.com>
2025-04-06 17:53:50 +0100

    Role Playing Game revision: 16

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -62,3 +62,24 @@ const weapons = [
 ];
 
 console.log(weapons[2]);
+
+// dse r monsters but dragon is vip
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15,
+    },
+
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60,
+    },
+
+    {
+        name: "dragon",
+        level: 20,
+        health: 300,
+    },
+];
