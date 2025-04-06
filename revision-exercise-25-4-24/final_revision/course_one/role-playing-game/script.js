cdffdaa2ee5214056c15edec49382da02b8b7567
arabog <arababs2015@gmail.com>
2025-04-06 17:42:15 +0100

    Role Playing Game revision: 11

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -2,13 +2,12 @@
 
 // d experience,xp
 let xp = 0;
+
 let health = 100;
 let gold = 50;
 
 // current weapon index
 let currentWeapon = 0;
-
-// console.log(health);
 
 let fighting;
 let monsterHealth;
@@ -21,12 +20,10 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
-// console.log(button1, button2, button3);
-
-// modem 09058603826
-
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 
 const text = document.querySelector("#text");
+
+// modem 09058603826
