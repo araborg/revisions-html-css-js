36393ac99a53aedea509f0f38db332f0b2eea17e
arabog <arababs2015@gmail.com>
2025-04-06 17:34:11 +0100

    Role Playing Game revision: 8

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -16,6 +16,7 @@ let monsterHealth;
 // weapon collections
 let inventory = ["stick"];
 
+// btns
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
