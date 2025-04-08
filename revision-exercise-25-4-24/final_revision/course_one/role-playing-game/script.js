bbb948968c1abc1baafea560681f20dfd492ba7b
arabog <arababs2015@gmail.com>
2025-04-08 17:54:45 +0100

    Role Playing Game revision: 129

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -642,6 +642,10 @@ function pick(guess) {
     }
 
     text.innerText = `You picked ${guess}. Here are the random numbers: \n`;
+
+    for (let i = 0; i < 10; i++) {
+        text.innerText += `${numbers[i]} \n`;
+    }
 }
 
 function pickTwo() {}
