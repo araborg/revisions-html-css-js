53736eda0ebcc6fb12ae68908e5f0f1f47bfdef6
arabog <arababs2015@gmail.com>
2025-04-09 02:48:33 +0100

    Role Playing Game revision: 149

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -100,9 +100,9 @@ const locations = [
         /*
             Initial version of the buttons:
             <div id="controls">
-                <button id="button1">Go to store</button>
-                <button id="button2">Go to cave</button>
-                <button id="button3">Fight dragon</button>
+                <button id="button1"> Go to store </button>
+                <button id="button2"> Go to cave </button>
+                <button id="button3"> Fight dragon </button>
             </div>
         */
 
