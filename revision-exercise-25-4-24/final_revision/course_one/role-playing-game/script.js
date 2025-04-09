3e66ee4537087155a2ba655b0b03ef25100b20d1
arabog <arababs2015@gmail.com>
2025-04-09 02:54:36 +0100

    Role Playing Game revision: 152

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -106,7 +106,6 @@ const locations = [
         name: "town square",
 
         "button text": ["Go to store", "Go to cave", "Fight dragon"],
-
         "button functions": [goStore, goCave, fightDragon],
 
         text: 'You are in the town square. You see a sign that says "Store".',
@@ -156,7 +155,6 @@ const locations = [
 
         "button text": [
             "Go to town square",
-
             "Go to town square",
             "Go to town square",
         ],
