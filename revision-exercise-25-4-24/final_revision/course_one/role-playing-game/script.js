123345ec508342ef775267d5e57f74ce5ccbb714
arabog <arababs2015@gmail.com>
2025-04-11 22:59:16 +0100

    Role Playing Game personal revision: 110

--- revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/role-playing-game/script.js
@@ -1016,7 +1016,7 @@ function pick(guess) {
 
         text.innerText += `Right! You win 20 gold!`;
     } else {
-        if (playerHealth > 10) {
+        if (playerHealth > 0) {
             playerHealth -= 10;
 
             playerHealthText.innerText = playerHealth;
