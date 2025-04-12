002fff5115e550bdd65da36c08a1fe9fea8c7140
arabog <arababs2015@gmail.com>
2025-04-12 08:25:07 +0100

    Random Bg: 4

--- revision-exercise-25-4-24/final_revision/course_one/random_bg/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/random_bg/script.js
@@ -1,1 +1,14 @@
-const darkColorsArr = [];
+const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C",
+    "#616A6B",
+    "#4A235A",
+    "#4A5A",
+    "#2F4F4F",
+    "#0E4B5A",
+    "#0E4B",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+];
