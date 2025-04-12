436a5672aa9ae7cfed04e73b1a67957b9fc1b7a6
arabog <arababs2015@gmail.com>
2025-04-12 08:39:08 +0100

    Random Bg review: 2

--- revision-exercise-25-4-24/final_revision/course_one/random_bg/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/random_bg/script.js
@@ -43,3 +43,22 @@ const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
 const btn = document.querySelector("#btn");
 
 const color = [];
+
+
+#CD5C5C
+#F08080
+#FA8072
+#E9967A
+#FFA07A
+#DC143C
+#FF0000
+#B22222
+#8B0000
+
+
+#FFA07A
+#FF7F50
+#FF6347
+#FF4500
+#FF8C00
+#FFA500
