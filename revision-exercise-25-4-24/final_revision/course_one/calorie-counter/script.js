4be4f7c6d418aa9e73f36875fbb2231fd4efab28
arabog <arababs2015@gmail.com>
2025-04-12 14:51:24 +0100

    Calorie Counter revision: 2

--- revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
@@ -1,6 +1,13 @@
+// form
 const calorieCounter = document.getElementById("calorie-counter");
+
+// inputs
 const budgetNumberInput = document.getElementById("budget");
+
+// dropdown
 const entryDropdown = document.getElementById("entry-dropdown");
+
+// btns
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
