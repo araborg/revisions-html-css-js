89dbdd9ecd680878b4fdc5ca9637cc1594938417
arabog <arababs2015@gmail.com>
2025-04-13 13:56:11 +0100

    Calorie Counter revision: 45

--- revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
@@ -156,13 +156,17 @@ function getCaloriesFromInputs(list) {
     return calories;
 }
 
+// update UI
 function addEntry() {
     // #entryDropdown.value rep d id of d fieldset wc serves
     // as d container for anoda div with class input-container
     // With ds, d speciic input container will be selected.
     // It is like saying select an input container of class
     // .input-container inside a container with id of
-    // breakfast/lunch/dinner etc
+    // breakfast/lunch/dinner etc.
+
+    // N.B: Hwever, both d id and d class used were already
+    // available in d HTML file.
     const targetInputContainer = document.querySelector(
         `#${entryDropdown.value} .input-container`
     );
