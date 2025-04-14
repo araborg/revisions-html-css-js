490e49dff674ad7c0e1e23edaa4115161087ca18
arabog <arababs2015@gmail.com>
2025-04-14 21:08:30 +0100

    Calorie Counter revision: 82

--- revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
@@ -283,4 +283,7 @@ function clearForm() {
 
 clearButton.addEventListener("click", clearForm);
 
-const arr = "1,2,3,4,5,6";
+const arr = "1, 2, 3, 4, 5, 6";
+const newArr = Array.from(arr);
+
+console.log(newArr);
