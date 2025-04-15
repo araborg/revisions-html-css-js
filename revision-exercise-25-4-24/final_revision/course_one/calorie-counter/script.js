9953083814b281a9a68f11721824526d2ec0b654
arabog <arababs2015@gmail.com>
2025-04-15 20:47:38 +0100

    Calorie Counter revision: 137

--- revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/calorie-counter/script.js
@@ -359,10 +359,12 @@ function addEntry() {
     // console.log(entryAdded);
 
     // d children
-    const entryId = entryAdded.querySelectorAll("input[type=number]");
+    const entryId = entryAdded.querySelectorAll("input[type=text]");
 
     const HTMLString = `
-        <label for="${entryDropdown.value}-${entryId}-Name">Entry ${entryId} Name</label>
+        <label for="${entryDropdown.value}-${entryId}-Name">
+            Entry ${entryId} Name
+        </label>
         <input
             type="number"
             id="${entryDropdown.value}-${entryId}-Name"
