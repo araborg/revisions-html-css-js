f03c15ec5e690c5166cd67ec7eba5528a9e409e2
arabog <arababs2015@gmail.com>
2025-04-16 15:46:18 +0100

    rock_and_paper: 8

--- revision-exercise-25-4-24/final_revision/course_one/rock_paper_scissors/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/rock_paper_scissors/script.js
@@ -13,7 +13,8 @@ const rockBtn = document.getElementById("rock-btn");
 const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 
-console.log(rockBtn, paperBtn, scissorsBtn);
+// console.log(rockBtn, paperBtn, scissorsBtn);
+
 // fxns:
 function getRandomComputerResult() {}
 
