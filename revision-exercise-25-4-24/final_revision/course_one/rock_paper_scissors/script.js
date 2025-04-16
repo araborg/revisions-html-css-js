26455826312f8849c284c4fc422b9170c1e56441
arabog <arababs2015@gmail.com>
2025-04-16 15:41:31 +0100

    rock_and_paper: 4

---
+++ revision-exercise-25-4-24/final_revision/course_one/rock_paper_scissors/script.js
@@ -0,0 +1,26 @@
+// html elemts
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+// fxns:
+function getRandomComputerResult() {}
+
+function hasPlayerWonTheRound(player, computer) {}
+
+function getRoundResults(userOption) {}
+
+function showResults(userOption) {}
+
+function resetGame() {}
+
+rockBtn.addEventListener("click", function () {});
+
+paperBtn.addEventListener("click", function () {});
+
+scissorsBtn.addEventListener("click", function () {});
