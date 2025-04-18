d3bec25e1941f017b1bf4ee1b6850777250045c7
arabog <arababs2015@gmail.com>
2025-04-18 23:44:32 +0100

    Music Player: 02

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -1,11 +1,3 @@
-const playlistSongs = document.getElementById("playlist-songs");
-
-const previousButton = document.getElementById("previous");
-const playButton = document.getElementById("play");
-const pauseButton = document.getElementById("pause");
-const nextButton = document.getElementById("next");
-const shuffleButton = document.getElementById("shuffle");
-
 const allSongs = [
     {
         id: 0,
@@ -14,6 +6,7 @@ const allSongs = [
         duration: "4:25",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
     },
+
     {
         id: 1,
         title: "Can't Stay Down",
@@ -21,6 +14,7 @@ const allSongs = [
         duration: "4:15",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stay-down.mp3",
     },
+
     {
         id: 2,
         title: "Still Learning",
@@ -28,6 +22,7 @@ const allSongs = [
         duration: "3:51",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3",
     },
+
     {
         id: 3,
         title: "Cruising for a Musing",
@@ -35,6 +30,7 @@ const allSongs = [
         duration: "3:34",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cruising-for-a-musing.mp3",
     },
+
     {
         id: 4,
         title: "Never Not Favored",
@@ -42,6 +38,7 @@ const allSongs = [
         duration: "3:35",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
     },
+
     {
         id: 5,
         title: "From the Ground Up",
@@ -49,6 +46,7 @@ const allSongs = [
         duration: "3:12",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
     },
+
     {
         id: 6,
         title: "Walking on Air",
@@ -56,6 +54,7 @@ const allSongs = [
         duration: "3:25",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
     },
+
     {
         id: 7,
         title: "Can't Stop Me. Can't Even Slow Me Down.",
@@ -63,6 +62,7 @@ const allSongs = [
         duration: "3:52",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
     },
+
     {
         id: 8,
         title: "The Surest Way Out is Through",
@@ -70,6 +70,7 @@ const allSongs = [
         duration: "3:10",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
     },
+
     {
         id: 9,
         title: "Chasing That Feeling",
