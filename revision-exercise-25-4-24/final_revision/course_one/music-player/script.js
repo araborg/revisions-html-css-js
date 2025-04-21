61f4f6612b9bc18c59a4cd95cc2c73d8362d9039
arabog <arababs2015@gmail.com>
2025-04-21 02:03:34 +0100

    Music Player: 24

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -109,6 +109,15 @@ const playSong = (id) => {
     audio.src = song.src;
     audio.title = song.title;
 
+    if (
+        userData?.currentSong === null ||
+        userData?.currentSong.id !== song.id
+    ) {
+        audio.currentTime = 0;
+    } else {
+        audio.currentTime = userData?.songCurrentTime;
+    }
+
     console.log(song);
 };
 
