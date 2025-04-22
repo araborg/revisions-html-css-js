a71b51139a1db588a25b4eeb20ab8804fee856ec
arabog <arababs2015@gmail.com>
2025-04-22 07:33:28 +0100

    Music Player: 94

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -156,7 +156,21 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songHTML;
 };
 
-const sortSongs = () => {};
+const sortSongs = () => {
+    userData?.songs.sort((a, b) => {
+        if (a.title < b.title) {
+            return -1;
+        }
+
+        if (a.title > b.title) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    return userData?.songs;
+};
 
 renderSongs(userData?.songs);
 
