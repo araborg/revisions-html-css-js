626d97699e6e5f70d358db3e0e9d929576317a66
arabog <arababs2015@gmail.com>
2025-04-23 08:01:56 +0100

    Music Player: 128

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -318,13 +318,13 @@ shuffleBtn.addEventListener("click", shuffle);
 
 const deleteSong = (id) => {
     // deleting a playing song
-    if (userData?.currentSong?.id === id) {
-        userData.currentSong = null;
-        userData.songCurrentTime = 0;
+    // if (userData?.currentSong?.id === id) {
+    //     userData.currentSong = null;
+    //     userData.songCurrentTime = 0;
 
-        pauseSong();
-        setPlayerDisplay();
-    }
+    //     pauseSong();
+    //     setPlayerDisplay();
+    // }
 
     // temporary modificatn of d userData.songs
     // let songs dt have diff ids apart from given
