4cc54b55708d254fc6074f8a84ca53c0f356c358
arabog <arababs2015@gmail.com>
2025-04-24 21:53:20 +0100

    Music Player Revisn: 31

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -584,9 +584,19 @@ const renderSongs = (array) => {
     playlistSongs.insertAdjacentHTML("beforeend", songHTML);
 };
 
-const sortSongs = () => {};
+const sortSongs = () => {
+    userData?.songs.sort((a, b) => {
+        if (a.title > b.title) return 1;
 
-renderSongs(userData?.songs);
+        if (a.title < b.title) return -1;
+
+        return 0;
+    });
+
+    return userData?.songs;
+};
+
+renderSongs(sortSongs());
 
 const highlightCurrentSong = () => {};
 
