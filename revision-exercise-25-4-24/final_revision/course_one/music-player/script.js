0becf95db86cebb7d8c0d2df1f904d2a6fb340e0
arabog <arababs2015@gmail.com>
2025-04-24 16:29:01 +0100

    Music Player Revisn: 29

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -556,14 +556,15 @@ const renderSongs = (array) => {
     const songHTML = array
         .map(
             (song) => `
+
             <li id="song-${song.id}" class="playlist-song">
                 <button
                     class="playlist-song-info"
                     onclick="playSong(${song.id})"
                 >
-                    <span>${song.title}</span>
-                    <span>${song.artist}</span>
-                    <span>${song.duration}</span>
+                    <span class="playlist-song-title">${song.title}</span>
+                    <span class="playlist-song-artist">${song.artist}</span>
+                    <span class="playlist-song-duration">${song.duration}</span>
                 </button>
 
                 <button
