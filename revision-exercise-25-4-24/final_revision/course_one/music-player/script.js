7ff95c80354759b52cb01cd4d096d21e16af9647
arabog <arababs2015@gmail.com>
2025-04-26 15:09:59 +0100

    Music Player Revisn: 169

--- revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
+++ revision-exercise-25-4-24/final_revision/course_one/music-player/script.js
@@ -783,3 +783,8 @@ const setPlayerDisplay = () => {
 };
 
 // mk d next song play at d end of the current song
+audio.addEventListener("ended", () => {
+    if (userData?.currentSong) {
+        console.log(`${userData?.currentSong} has ended`);
+    }
+});
