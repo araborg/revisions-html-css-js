48cfc117d6b0389597ab915bfd38f4fd46a65fbb
arabog <arababs2015@gmail.com>
2025-04-26 21:26:11 +0100

    Palindrome 3

--- revision-exercise-25-4-24/final_revision/capstone_projects/pallindrome/script.js
+++ revision-exercise-25-4-24/final_revision/capstone_projects/pallindrome/script.js
@@ -8,31 +8,12 @@ const formattedValueFxn = (str) => {
     return str.replace(regex, "");
 };
 
-// console.log(formattedValueFxn("A"));
-// console.log(formattedValueFxn("eye"));
-// console.log(formattedValueFxn("_eye"));
-// console.log(formattedValueFxn("race car"));
-// console.log(formattedValueFxn("not a palindrome"));
-// console.log(formattedValueFxn("A man, a plan, a canal. Panama"));
-// console.log(formattedValueFxn("never odd or even"));
-// console.log(formattedValueFxn("nope"));
-// console.log(formattedValueFxn("almostomla"));
-// console.log(formattedValueFxn("My age is 0, 0 si ega ym"));
-// console.log(formattedValueFxn("1 eye for of 1 eye");
-// console.log(formattedValueFxn("0_0 (: /- :) 0-0");
-// console.log(formattedValueFxn("five|_/|four");
-// console.log(formattedValueFxn("ar44ra");
-// console.log(formattedValueFxn("ar4ra");
-// console.log(formattedValueFxn("ar4r");
-
 const checkInputValue = () => {
     const inputValue = textInput.value;
 
     const formattedValue = formattedValueFxn(inputValue);
 
     const splitString = formattedValue.split("");
-
-    // console.log(inputValue, splitString);
 
     if (!inputValue) {
         alert("Please input avalue.");
