c677c77eba00a18b068621de93f2e5ece96fb04f
arabog <arababs2015@gmail.com>
2025-04-26 20:40:02 +0100

    Palindrome 20

--- revision-exercise-25-4-24/final_revision/capstone_projects/pallindrome/script.js
+++ revision-exercise-25-4-24/final_revision/capstone_projects/pallindrome/script.js
@@ -8,22 +8,22 @@ const formattedValueFxn = (str) => {
     return str.replace(regex, "");
 };
 
-console.log(formattedValueFxn("A"));
-console.log(formattedValueFxn("eye"));
-console.log(formattedValueFxn("_eye"));
-console.log(formattedValueFxn("race car"));
-console.log(formattedValueFxn("not a palindrome"));
-console.log(formattedValueFxn("A man, a plan, a canal. Panama"));
-console.log(formattedValueFxn("never odd or even"));
-console.log(formattedValueFxn("nope"));
-console.log(formattedValueFxn("almostomla"));
-console.log(formattedValueFxn("My age is 0, 0 si ega ym"));
-console.log(formattedValueFxn("1 eye for of 1 eye"));
-console.log(formattedValueFxn("0_0 (: /- :) 0-0"));
-console.log(formattedValueFxn("five|_/|four"));
-console.log(formattedValueFxn("ar44ra"));
-console.log(formattedValueFxn("ar4ra"));
-console.log(formattedValueFxn("ar4r"));
+// console.log(formattedValueFxn("A"));
+// console.log(formattedValueFxn("eye"));
+// console.log(formattedValueFxn("_eye"));
+// console.log(formattedValueFxn("race car"));
+// console.log(formattedValueFxn("not a palindrome"));
+// console.log(formattedValueFxn("A man, a plan, a canal. Panama"));
+// console.log(formattedValueFxn("never odd or even"));
+// console.log(formattedValueFxn("nope"));
+// console.log(formattedValueFxn("almostomla"));
+// console.log(formattedValueFxn("My age is 0, 0 si ega ym"));
+// console.log(formattedValueFxn("1 eye for of 1 eye"));
+// console.log(formattedValueFxn("0_0 (: /- :) 0-0"));
+// console.log(formattedValueFxn("five|_/|four"));
+// console.log(formattedValueFxn("ar44ra"));
+// console.log(formattedValueFxn("ar4ra"));
+// console.log(formattedValueFxn("ar4r"));
 
 const checkInputValue = () => {
     const inputValue = textInput.value;
