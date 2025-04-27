3afac36a43b7b2936d479d75f25c81ca1be3a9fe
arabog <arababs2015@gmail.com>
2025-04-27 18:45:04 +0100

    Date formatter: 17

--- revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
@@ -22,5 +22,9 @@ dateOptionsSelectElement.addEventListener("change", () => {
                 .split("-")
                 .reverse()
                 .join("-");
+            break;
+
+        case "mm-dd-yyyy-h-mm":
+            currentDateParagraph.textContent = ``;
     }
 });
