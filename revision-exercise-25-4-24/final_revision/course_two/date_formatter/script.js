a46ab9806e41e06b9b72d5695e7d8f2a252a94e5
arabog <arababs2015@gmail.com>
2025-04-27 17:35:08 +0100

    Date formatter: 6

--- revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
@@ -1,4 +1,9 @@
 const dateOptionsSelectElement = document.getElementById("date-options");
 const currentDateParagraph = document.getElementById("current-date");
 
-console.log(dateOptionsSelectElement, currentDateParagraph);
+// console.log(dateOptionsSelectElement, currentDateParagraph);
+
+// date ppties and methods
+const date = new Date();
+
+console.log(date);
