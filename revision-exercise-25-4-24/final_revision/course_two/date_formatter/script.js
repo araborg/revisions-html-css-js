a0846ed53704c0151e0c91b90f360c2a14fa29b8
arabog <arababs2015@gmail.com>
2025-04-27 17:38:41 +0100

    Date formatter: 8

--- revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/date_formatter/script.js
@@ -4,9 +4,16 @@ const currentDateParagraph = document.getElementById("current-date");
 // console.log(dateOptionsSelectElement, currentDateParagraph);
 
 // date ppties and methods
+// days
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 
-console.log(date, day, month, year);
+// time
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+//
+
+console.log(date, day, month, year, hours, minutes);
