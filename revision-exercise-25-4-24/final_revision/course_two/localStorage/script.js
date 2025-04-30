f3b7e55ee28319d73922d9ace52e6abf3ec5bdcc
arabog <arababs2015@gmail.com>
2025-04-30 16:20:26 +0100

    local storage: 9

--- revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
@@ -5,6 +5,11 @@ const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 
+// inputs
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
+
 // dialog btns
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const cancleBtn = document.getElementById("cancle-btn");
@@ -12,4 +17,5 @@ const discardBtn = document.getElementById("discard-btn");
 
 // task container
 const tasksContainer = document.getElementById("tasks-container");
+
 console.log(openTaskFormBtn);
