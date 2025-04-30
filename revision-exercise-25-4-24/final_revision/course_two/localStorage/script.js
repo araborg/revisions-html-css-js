56eb12f24138c4f9f580386a48c3642279e08231
arabog <arababs2015@gmail.com>
2025-04-30 16:18:11 +0100

    local storage: 8

--- revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
@@ -8,5 +8,8 @@ const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 // dialog btns
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const cancleBtn = document.getElementById("cancle-btn");
+const discardBtn = document.getElementById("discard-btn");
 
+// task container
+const tasksContainer = document.getElementById("tasks-container");
 console.log(openTaskFormBtn);
