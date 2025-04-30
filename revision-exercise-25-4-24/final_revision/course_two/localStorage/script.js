daf0371fc497ec33e21e6144183898bfb815808b
arabog <arababs2015@gmail.com>
2025-04-30 16:11:17 +0100

    local storage: 4

--- revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/localStorage/script.js
@@ -1,4 +1,7 @@
-const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const taskForm = document.getElementById("task-form");
+
+// btns
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 
 console.log(openTaskFormBtn);
