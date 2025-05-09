8ff9f71056fbd5563db9751a3b055ba990b92b35
arabog <arababs2015@gmail.com>
2025-05-09 15:58:25 +0100

    recursion revision: 06

--- revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
@@ -4,11 +4,17 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 const animationContainer = document.getElementById("animation-container");
 
+// addElDelay: delays d msg for sm millisec b4 showing
+// showMsgDelay: keeps d msg for sm millisec
+// removeElDelay: remove d msg after sm millisec
+
 const animationData = [
     {
         inputVal: 5,
         addElDelay: 1000,
+
         msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+
         showMsgDelay: 15000,
         removeElDelay: 20000,
     },
@@ -16,7 +22,9 @@ const animationData = [
     {
         inputVal: 2,
         addElDelay: 1500,
+
         msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+
         showMsgDelay: 10000,
         removeElDelay: 15000,
     },
@@ -24,7 +32,9 @@ const animationData = [
     {
         inputVal: 1,
         addElDelay: 2000,
+
         msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+
         showMsgDelay: 5000,
         removeElDelay: 10000,
     },
