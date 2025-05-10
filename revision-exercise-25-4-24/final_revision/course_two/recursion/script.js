1951df077fec5613c7b3f0e7d1809c3300a472fc
arabog <arababs2015@gmail.com>
2025-05-10 12:43:24 +0100

    recursion revision: 78

--- revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
@@ -239,6 +239,9 @@ const numberInput = document.getElementById("number-input");
 
 const convertBtn = document.getElementById("convert-btn");
 
+const output = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+
 // Method 1:
 
 // Method 2:
