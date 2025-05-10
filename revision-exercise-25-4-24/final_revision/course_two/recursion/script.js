9f161381adced65e8c17797326d812a69c889bf2
arabog <arababs2015@gmail.com>
2025-05-10 11:26:07 +0100

    recursion revision: 72

--- revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/recursion/script.js
@@ -115,7 +115,6 @@ const decimalToBinary = (input) => {
 // How d animation is shown: 5 -->  2  --> 1
 // How d animation is removed: 1 -->  2  --> 5
 
-*/
 
 const animationData = [
     {
@@ -228,3 +227,41 @@ numberInput.addEventListener("keydown", (e) => {
         getInputValue();
     }
 });
+
+
+
+*/
+
+// Revision:
+
+const animationData = [
+    {
+        inputVal: 5,
+        addElDelay: 1000,
+
+        msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+
+        showMsgDelay: 15000,
+        removeElDelay: 20000,
+    },
+
+    {
+        inputVal: 2,
+        addElDelay: 1500,
+
+        msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+
+        showMsgDelay: 10000,
+        removeElDelay: 15000,
+    },
+
+    {
+        inputVal: 1,
+        addElDelay: 2000,
+
+        msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+
+        showMsgDelay: 5000,
+        removeElDelay: 10000,
+    },
+];
