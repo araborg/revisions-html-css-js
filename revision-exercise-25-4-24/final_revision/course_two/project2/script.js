3925d8fd48f6a3de234734dc54e3cad529e79ec5
arabog <arababs2015@gmail.com>
2025-05-11 19:10:39 +0100

    recursion revision: 188

--- revision-exercise-25-4-24/final_revision/course_two/project2/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/project2/script.js
@@ -25,7 +25,7 @@ const convertToRoman = () => {
     let output = [];
 
     romans.forEach((data) => {
-        if (input <= data[1]) {
+        if (input >= data[1]) {
             output.push(data[0]);
 
             input -= data[1];
