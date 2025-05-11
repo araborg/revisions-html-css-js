2a448d28ec8489277d2f14cc8be68d058fc4bb50
arabog <arababs2015@gmail.com>
2025-05-11 18:27:21 +0100

    recursion revision: 171

--- revision-exercise-25-4-24/final_revision/course_two/project2/script.js
+++ revision-exercise-25-4-24/final_revision/course_two/project2/script.js
@@ -2,3 +2,21 @@ const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 
 const output = document.getElementById("output");
+
+const convertToRoman = () => {
+    const romans = [
+        [M, 1000],
+        [CM, 900],
+        [D, 500],
+        [CD, 400],
+        [C, 100],
+        [XC, 90],
+        [L, 50],
+        [XL, 40],
+        [X, 10],
+        [IX, 9],
+        [V, 5],
+        [IV, 4],
+        [I, 1],
+    ];
+};
