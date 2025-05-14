0581bb4d26b937a6d2ebf6c75aa516895b807a30
arabog <arababs2015@gmail.com>
2025-05-14 20:34:25 +0100

    Basic Algorithm: Number Sorter: 20

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -22,7 +22,9 @@ const updateUI = (array = []) => {
 
 // array: 8, 2, 4, 1, 3
 
-// index: 0, 1, 2, 3, 4
+// index i: 0, 1, 2, 3, 4
+
+// index j: 0, 1, 2, 3, 4
 
 // bubble sort
 const bubbleSort = (array) => {
