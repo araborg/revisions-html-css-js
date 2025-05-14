f75e812a5b04b730f8c500f08b67748e8f349a2a
arabog <arababs2015@gmail.com>
2025-05-14 09:14:25 +0100

    Basic Algorithm: Number Sorter: 11

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -3,7 +3,9 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (e) => {
     e.preventDefault();
 
-    const inputValues = [...document.getElementsByClassName("values-dropdown")];
+    const inputValues = [
+        ...document.getElementsByClassName("values-dropdown"),
+    ].map((dropdown) => Number(dropdown.value));
 
     console.log(inputValues);
 };
