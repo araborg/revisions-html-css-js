6e722a3f52797b4c84bba10407a36bb2887fdcb8
arabog <arababs2015@gmail.com>
2025-05-16 18:53:47 +0100

    Basic Algorithm: Number Sorter: 42

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -35,7 +35,7 @@ const bubbleSort = (array) => {
         //
 
         for (let j = 0; j < array.length - 1; j++) {
-            // inner loop runs 3, 2, 1, 0 times
+            // inner loop runs 0, 1, 2, 3 times
 
             if (array[j] > array[j + 1]) {
                 // j is d current index while j+1 is
