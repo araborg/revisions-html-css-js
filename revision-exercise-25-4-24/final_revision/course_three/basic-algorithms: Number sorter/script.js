ef0a7bc3154aacdf67c8474b0f90dbba3b6c5370
arabog <arababs2015@gmail.com>
2025-05-17 18:41:32 +0100

    Basic Algorithm: Number Sorter: 94

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -146,11 +146,13 @@ const selectionSort = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         let minIndex = i;
 
-        for (let j = 1; j < arr.length; j++) {
-            if (arr[j] > arr[minIndex]) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
+
+        console.log(minIndex);
     }
 };
 
