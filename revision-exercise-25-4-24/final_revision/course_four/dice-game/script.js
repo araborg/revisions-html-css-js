77f85526c1fe6d75922842f40510e3d9c6aa3fe9
arabog <arababs2015@gmail.com>
2025-05-18 19:22:56 +0100

    Basic Algorithm: dice game 6

--- revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
@@ -1,3 +1,16 @@
 const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options");
+const scoreSpans = document.querySelectorAll(".score span");
 
-console.log(listOfAllDice);
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesBtn = document.getElementById("rule-btn");
+const rulesContainer = document.querySelector(".rules-container");
+
+console.log(scoreInputs);
