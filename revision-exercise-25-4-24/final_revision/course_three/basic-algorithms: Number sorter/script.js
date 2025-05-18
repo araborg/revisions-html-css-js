921c2c33432d915107fdb0e94d5b590c76f49865
arabog <arababs2015@gmail.com>
2025-05-18 12:04:52 +0100

    Basic Algorithm: Number Sorter review: 2

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -1,1 +1,9 @@
 const sortBtn = document.getElementById("sort");
+
+const sortValue = (e) => {
+    e.preventDefault();
+
+    console.log("Hello");
+};
+
+sortBtn.addEventListener("click", sortValue);
