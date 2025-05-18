ca665eaa91e66b3a5b8891e514027847d0c388b3
arabog <arababs2015@gmail.com>
2025-05-18 13:21:33 +0100

    Basic Algorithm: Number Sorter review: 17

--- revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/basic-algorithms: Number sorter/script.js
@@ -21,9 +21,15 @@ const displayUI = (arr) => {
 const bubbleSort = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - 1; j++) {
-            console.log(i, j);
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
+
+    return arr;
 };
 
 sortBtn.addEventListener("click", sortValue);
