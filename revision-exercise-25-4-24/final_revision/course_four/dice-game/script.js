d35264d51ee34ea44c031264cde424920acb4df6
arabog <arababs2015@gmail.com>
2025-05-20 12:51:39 +0100

    Basic Algorithm: Number Sorter: 103

--- revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
@@ -3,8 +3,9 @@ const listOfAllDice = document.querySelectorAll(".die");
 const scoreInputs = document.querySelectorAll("#score-options input");
 const scoreSpans = document.querySelectorAll("#score-options span");
 
-const roundElement = document.getElementById("current-round");
 const rollsElement = document.getElementById("current-round-rolls");
+const roundElement = document.getElementById("current-round");
+
 const totalScoreElement = document.getElementById("total-score");
 const scoreHistory = document.getElementById("score-history");
 
