563bbd3577e75ccdf47b8d35f1a681256c40abe2
arabog <arababs2015@gmail.com>
2025-05-20 15:33:59 +0100

    Basic Algorithm: Dice: 3

--- revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
@@ -252,3 +252,6 @@ const rulesBtn = document.getElementById("rules-btn");
 const rulesContainer = document.getElementById("rules-container");
 
 const dice = document.querySelectorAll(".die");
+
+const currentRolls = document.getElementById("current-round-rolls");
+const currentRound = document.getElementById("current-round");
