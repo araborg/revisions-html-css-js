5393f25bdeada9a08f226f6e6cb383282869f284
arabog <arababs2015@gmail.com>
2025-05-20 14:27:52 +0100

    Basic Algorithm: Number Sorter: 125

--- revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/dice-game/script.js
@@ -155,6 +155,9 @@ const checkForStraights = (arr) => {
 
     const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
     const uniqueNumbersStr = uniqueNumbersArr.join("");
+
+    const smallStraightsArr = ["1234", "2345", "3456"];
+    const largeStraightsArr = ["12345", "23456"];
 };
 
 // btns
