6421296f462eab492802d9b48bc5d20f0020f89f
arabog <arababs2015@gmail.com>
2025-05-28 08:43:36 +0100

    OOP: Shopping Cart: 06

--- revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
@@ -1,4 +1,5 @@
 const cartBtn = document.getElementById("cart-btn");
 const showOrHideCart = document.getElementById("show-hide-cart");
 
+const cartContainer = document.getElementById("cart-container");
 console.log(cartBtn, showOrHideCart);
