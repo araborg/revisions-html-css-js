f8ca531905facbd6e1f7d8cb16200967afe041f6
arabog <arababs2015@gmail.com>
2025-05-28 08:45:40 +0100

    OOP: Shopping Cart: 07

--- revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
@@ -2,4 +2,14 @@ const cartBtn = document.getElementById("cart-btn");
 const showOrHideCart = document.getElementById("show-hide-cart");
 
 const cartContainer = document.getElementById("cart-container");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+const productsContainer = document.getElementById("products-container");
+
+/*
+
+
+
+
+*/
+
 console.log(cartBtn, showOrHideCart);
