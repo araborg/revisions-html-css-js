f58f4ab873d4baf89a7ff3d02196af093f10c9ed
arabog <arababs2015@gmail.com>
2025-05-28 08:55:44 +0100

    OOP: Shopping Cart: 14

--- revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
@@ -1,17 +1,3 @@
-const cartBtn = document.getElementById("cart-btn");
-const showOrHideCart = document.getElementById("show-hide-cart");
-
-const cartContainer = document.getElementById("cart-container");
-const clearCartBtn = document.getElementById("clear-cart-btn");
-const productsContainer = document.getElementById("products-container");
-
-const totalItems = document.getElementById("total-items");
-const subTotal = document.getElementById("subtotal");
-const taxes = document.getElementById("taxes");
-const total = document.getElementById("total");
-
-const dessertCardContainer = document.getElementById("dessert-card-container");
-
 const isCartShowing = false;
 
 const products = [
