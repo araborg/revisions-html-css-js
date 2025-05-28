f164b8767ec42d4f9c7dd3a1a82299e9ce227fba
arabog <arababs2015@gmail.com>
2025-05-28 09:07:57 +0100

    OOP: Shopping Cart: 17

--- revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
@@ -106,7 +106,19 @@ const products = [
 products.forEach(({ id, name, price, category }) => {
     dessertCards.innerHTML += `
         <div class="dessert-card">
+            <h2>${name}</h2>
+            <p class="dessert-price">$${price}</p>
+            
+            <p class="product-category">
+                Category: ${category}
+            </p>
 
+            <button
+                id="${id}"
+                class="btn add-to-cart-btn"
+            >
+                Add to cart
+            </button>
         </div>
     `;
 });
