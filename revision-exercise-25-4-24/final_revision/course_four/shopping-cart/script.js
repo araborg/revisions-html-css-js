631eefebe1964d3a037d9f740bb0ca225f7ea210
arabog <arababs2015@gmail.com>
2025-05-29 16:27:43 +0100

    OOP: Shopping Cart: 73

--- revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/shopping-cart/script.js
@@ -208,7 +208,6 @@ class ShoppingCart {
 
         cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
         cartTaxes.textContent = `$${tax.toFixed(2)}`;
-
         cartTotal.textContent = `${this.total.toFixed(2)}`;
 
         return this.total;
