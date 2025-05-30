16160142e2a071996c5955b7ecf79b8824524ec9
arabog <arababs2015@gmail.com>
2025-05-30 09:47:38 +0100

    OOP: Platformer Game: 3

--- revision-exercise-25-4-24/final_revision/course_four/platformer-game/script.js
+++ revision-exercise-25-4-24/final_revision/course_four/platformer-game/script.js
@@ -1,2 +1,10 @@
 const startBtn = document.getElementById("start-btn");
+
 const startScreen = document.querySelector(".start-screen");
+
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+
+const canvas = document.getElementById("canvas");
+
+// console.log(checkpointScreenMessage);
