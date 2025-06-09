4dc59e8b384fee7be2001b2f96d367c5fe3aaf77
araborg <arababs2015@gmail.com>
2025-06-09 12:45:28 +0100

    rounding up: projects 2

--- revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
@@ -1,1 +1,19 @@
 const getMean = (array) => array.reduce((acc, el) => acc+el,0 )/array.length
+
+/*
+Prettier
+
+Go to setting and set these:
+default formatter: prettier
+format on save: on
+autosave: onfocus change
+
+
+Monokai Pro
+VS Code settings
+
+word wrap
+format on save: on
+Install a formatter
+
+*/ 
