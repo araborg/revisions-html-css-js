2ebefa8d7d43f1c7d20b913253f36956e4afa251
araborg <arababs2015@gmail.com>
2025-06-09 13:09:00 +0100

    rounding up: projects 5

--- revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
@@ -1,24 +1,2 @@
-const getMean = (array) => array.reduce((acc, el) => acc+el,0 )/array.length
-
-/*
-Prettier
-
-Go to setting and set these:
-default formatter: prettier
-format on save: on
-autosave: onfocus change
-tap size: 4
-
-One Monokai
-VS Code settings
-file icon theme: seti
-
-Image preview
-Color highlight
-
-
-word wrap
-format on save: on
-Install a formatter
-
-*/ 
+const getMean = (array) =>
+  array.reduce((acc, el) => acc + el, 0) / array.length;
