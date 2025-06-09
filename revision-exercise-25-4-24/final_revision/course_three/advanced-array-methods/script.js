aca20721422498af4fced2ee69888fc2204bb358
araborg <arababs2015@gmail.com>
2025-06-09 18:29:58 +0100

    rounding up: projects 9

--- revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
@@ -1,20 +1,23 @@
 const getMean = (array) =>
-  array.reduce((acc, el) => acc + el, 0) / array.length;
+    array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.slice().sort((a, b) => a - b);
+    const sorted = array.slice().sort((a, b) => a - b);
 
-  const median =
-    array.length % 2 === 0
-      ? getMean([
-          sorted[array.length / 2 - 1],
-          sorted[array.length / 2],
-        ])
-      : sorted[Math.floor(array.length / 2)];
+    const median =
+        // divisn happens bf minus
+        array.length % 2 === 0
+            ? getMean([
+                  sorted[array.length / 2 - 1],
+                  sorted[array.length / 2],
+              ])
+            : sorted[Math.floor(array.length / 2)];
 
-  return median;
+    return median;
 };
 
-const array = [1, 2, 3, 4, 5];
+const getMode = () => {};
+
+const array = [1, 2, 3, 4, 5, 6];
 
 console.log(getMedian(array));
