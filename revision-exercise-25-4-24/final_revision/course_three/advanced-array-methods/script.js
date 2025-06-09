e6d88561a329ec7f6ec5b52e93f890c5b7e2cfe0
araborg <arababs2015@gmail.com>
2025-06-09 13:13:14 +0100

    rounding up: projects 7

--- revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/advanced-array-methods/script.js
@@ -1,4 +1,6 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const getMedian = (array) => {};
+const getMedian = (array) => {
+  const sorted = array.slice().sort((a, b) => a - b);
+};
