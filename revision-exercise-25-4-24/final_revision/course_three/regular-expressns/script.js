1e8eb1f963274a222406d29519a9bdecb5c971f6
araborg <arababs2015@gmail.com>
2025-06-10 06:19:53 +0100

    Regex: 2

---
+++ revision-exercise-25-4-24/final_revision/course_three/regular-expressns/script.js
@@ -0,0 +1,7 @@
+const messageInput =
+	document.getElementById("message-input");
+const checkMessageButton =
+	document.getElementById("chech-message");
+const result = document.getElementById("result");
+
+console.log(messageInput, checkMessageButton, result);
