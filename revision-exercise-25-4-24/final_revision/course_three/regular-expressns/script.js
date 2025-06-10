e74ef073a185d95f65948a617ca9e58e5c821b1b
araborg <arababs2015@gmail.com>
2025-06-10 06:25:42 +0100

    Regex: 4

--- revision-exercise-25-4-24/final_revision/course_three/regular-expressns/script.js
+++ revision-exercise-25-4-24/final_revision/course_three/regular-expressns/script.js
@@ -4,3 +4,9 @@ const checkMessageButton = document.getElementById(
 	"check-message-btn"
 );
 const result = document.getElementById("result");
+
+const helpRegex = /please help|assist me/i;
+const dollarRegex =
+	/[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
