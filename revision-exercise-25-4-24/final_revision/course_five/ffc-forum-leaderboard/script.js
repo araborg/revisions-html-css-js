842a0b63a2ea1778c82245e8e742fa7d44780aad
araborg <arababs2015@gmail.com>
2025-06-11 04:22:11 +0100

    fcc forum leaderboard: 3

--- revision-exercise-25-4-24/final_revision/course_five/ffc-forum-leaderboard/script.js
+++ revision-exercise-25-4-24/final_revision/course_five/ffc-forum-leaderboard/script.js
@@ -19,17 +19,21 @@ const allCategories = {
 		category: "Project Feedback",
 		className: "feedback",
 	},
+
 	417: {
 		category: "freeCodeCamp Support",
 		className: "support",
 	},
+
 	421: {category: "JavaScript", className: "javascript"},
 	423: {category: "HTML - CSS", className: "html-css"},
 	424: {category: "Python", className: "python"},
+
 	432: {
 		category: "You Can Do This!",
 		className: "motivation",
 	},
+
 	560: {
 		category: "Backend Development",
 		className: "backend",
