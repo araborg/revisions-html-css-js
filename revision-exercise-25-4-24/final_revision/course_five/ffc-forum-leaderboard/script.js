4d344bc3066aa944975ac1db8f2759b2f1e5f949
araborg <arababs2015@gmail.com>
2025-06-11 04:20:38 +0100

    fcc forum leaderboard: 1

---
+++ revision-exercise-25-4-24/final_revision/course_five/ffc-forum-leaderboard/script.js
@@ -0,0 +1,13 @@
+const postContainer = document.getElementById(
+	"posts-container"
+);
+
+const forumLatest =
+	"https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl =
+	"https://forum.freecodecamp.org/c/";
+const avatarUrl =
+	"https://sea1.discourse-cdn.com/freecodecamp";
+
+console.log(postContainer);
