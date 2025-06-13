0b9712b703c7b0c7c88627349bbf60db03653c8e
araborg <arababs2015@gmail.com>
2025-06-13 20:30:06 +0100

    pokemon: 15

--- revision-exercise-25-4-24/final_revision/course_five/project5/script.js
+++ revision-exercise-25-4-24/final_revision/course_five/project5/script.js
@@ -4,4 +4,11 @@ const searchInput = document.getElementById("search-input");
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonID = document.getElementById("pokemon-id");
 
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const spriteContainer = document.getElementById(
+	"sprite-container"
+);
+const types = document.getElementById("types");
+
 console.log(pokemonName, pokemonID);
