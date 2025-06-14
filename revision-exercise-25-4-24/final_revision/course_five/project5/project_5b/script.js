cd0e1814e36243a844567b229812d904c0d8856a
araborg <arababs2015@gmail.com>
2025-06-14 06:06:04 +0100

    pokemon-revision: 5

--- revision-exercise-25-4-24/final_revision/course_five/project5/project_5b/script.js
+++ revision-exercise-25-4-24/final_revision/course_five/project5/project_5b/script.js
@@ -5,3 +5,9 @@ https://rpg-creature-api.freecodecamp.rocks/api/creature/1
 
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
+
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonID = document.getElementById("pokemon-id");
+
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
