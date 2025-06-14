f0e92a9e95265b8345a99bf2acd62a269039dc53
araborg <arababs2015@gmail.com>
2025-06-14 08:42:09 +0100

    pokemon-revision: 38

--- revision-exercise-25-4-24/final_revision/course_five/project5/project_5b/script.js
+++ revision-exercise-25-4-24/final_revision/course_five/project5/project_5b/script.js
@@ -1,8 +1,3 @@
-/*
-https://rpg-creature-api.freecodecamp.rocks/api/creature/pyrolynx
-https://rpg-creature-api.freecodecamp.rocks/api/creature/1
-*/
-
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
 
